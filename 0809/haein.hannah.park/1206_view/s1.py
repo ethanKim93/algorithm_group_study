c464906ae44e29966436f0c95292dec29612dd80
박해인 <haein.hannah.park@gmail.com>
2021-08-09 22:33:00 +0900

    210809 Homework

--- 0809/haein.hannah.park/1206_view/s1.py
+++ 0809/haein.hannah.park/1206_view/s1.py
@@ -4,4 +4,21 @@ sys.stdin = open('input.txt')
 for tc in range(1, 11):
     L = int(input()) #길이
     buildings = list(map(int, input().split()))
-    
+
+    # 조망권이 확보 된 빌딩 : goodbuilding
+    goodbuilding = 0
+    for b in range(2, L-2):
+        r1 = buildings[b+1]
+        r2 = buildings[b+2]
+        l1 = buildings[b-1]
+        l2 = buildings[b-2]
+        # 주변 가장 큰 빌딩 높이 체크
+        tallest = max(r1, r2, l1, l2)
+        if buildings[b] > tallest:
+            goodbuilding += buildings[b] - tallest
+
+    print('#{} {}'.format(tc, goodbuilding))
+
+
+
+

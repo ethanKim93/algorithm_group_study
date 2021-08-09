6cede380dfae52d7bff484cba48a1de26d40c348
ghkdlxld <ohsg97@gmail.com>
2021-08-09 19:29:57 +0900

    LIST_1_homework

--- 0809/ohsg97/1206_view/s1.py
+++ 0809/ohsg97/1206_view/s1.py
@@ -4,5 +4,23 @@ sys.stdin = open('input.txt')
 for tc in range(1, 11):
     L = int(input())
     buildings = list(map(int, input().split()))
-    print(L)
-    print(buildings)
+
+    total = 0
+
+    for i in range(2, L-2):
+        height = buildings[i]
+
+        while height > buildings[i+1]:
+            if height > buildings[i + 1] and height > buildings[i + 2]:
+                if height > buildings[i - 1] and height > buildings[i - 2]:
+                    height -= 1
+                    total += 1
+                else:
+                    height -= 1
+                    continue
+
+            else:
+                height -= 1
+                continue
+
+    print('#{} {}'.format(tc,total))

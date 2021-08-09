4020a20c93a5b90c759f210cda898a9a072da6eb
sdh98429 <sdh98429@naver.com>
2021-08-09 17:43:21 +0900

    0809 일단 view만

--- 0809/sdh98429/1206_view/s1.py
+++ 0809/sdh98429/1206_view/s1.py
@@ -4,3 +4,12 @@ sys.stdin = open('input.txt')
 for tc in range(1,11):
     L = int(input())
     buildings = list(map(int,input().split()))
+    tot = 0
+    for b_x in range(2,L-2):
+        max_b = buildings[b_x - 2]
+        for i in [-1, 1, 2]:
+            if max_b < buildings[b_x + i]:
+                max_b = buildings[b_x + i]
+        if buildings[b_x] > max_b:
+            tot += (buildings[b_x] - max_b)
+    print('#{} {}'.format(tc, tot))
---
+++ 0809/sdh98429/gravity/s1.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for tc in range(1, T+1):
+    width = int(input())
+    heights = list(map(int, input().split()))
+    print(heights)

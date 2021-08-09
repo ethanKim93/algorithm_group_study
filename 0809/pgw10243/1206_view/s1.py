2f191daa2b9fdb6e3051101c99ca7724064e9986
Santiago-MZ <pgw10243@gmail.com>
2021-08-09 19:52:07 +0900

    1206_view 풀이 제출합니다.

--- 0809/pgw10243/1206_view/s1.py
+++ 0809/pgw10243/1206_view/s1.py
@@ -2,8 +2,26 @@ import sys
 sys.stdin = open("input.txt")
 
 for tc in range(1,11):
+    length = int(input())
+    buildings = list(map(int,input().split()))
 
+    i = 0
+    view = 0
+    while(i < length-4):
+        ind = max1 = 0
+        for idx, building in enumerate(buildings[i:i+5]):
+            if max1 < building:
+                max1 = building
+                ind = idx
 
+        max2 = 0
+        if ind == 2:
+            for building in buildings[i:i+2]+buildings[i+3:i+5]:
+                if max2 < building:
+                    max2 = building
+            i += 3
+            view += max1-max2
+        else:
+            i += 1
 
-
-    print('#{} {}'.format(tc,))
+    print('#{} {}'.format(tc,view))

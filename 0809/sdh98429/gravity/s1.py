355fe55fe548a6e5e3530853657533397214d264
sdh98429 <sdh98429@naver.com>
2021-08-09 22:37:14 +0900

    0809 gravity입니다.

--- 0809/sdh98429/gravity/s1.py
+++ 0809/sdh98429/gravity/s1.py
@@ -6,4 +6,16 @@ T = int(input())
 for tc in range(1, T+1):
     width = int(input())
     heights = list(map(int, input().split()))
-    print(heights)
+    # print(width)
+    max_gap = 0
+    for x_l in range(width-1):
+        gap = 0
+        for x_r in range(x_l+1, width):
+            if heights[x_l] > heights[x_r]:
+                gap += 1
+            else:
+                pass
+        if gap > max_gap:
+            max_gap = gap
+    print('#{} {}'.format(tc, max_gap))
+

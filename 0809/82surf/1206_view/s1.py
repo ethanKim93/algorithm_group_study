45217c7b32a92af0f124c70b95621234c25998b7
82surf <82surf@gmail.com>
2021-08-09 17:38:50 +0900

    1206 문제풀이 완료

--- 0809/82surf/1206_view/s1.py
+++ 0809/82surf/1206_view/s1.py
@@ -4,5 +4,29 @@ sys.stdin = open('input.txt')
 for tc in range(1, 11):
     L = int(input())
     buildings = list(map(int, input().split()))
-    print(L)
-    print(buildings)
+
+    result = 0
+    for i in range(2, L-1):
+        gaps = [0, 0, 0, 0]
+        gaps[0] = buildings[i] - buildings[i-2]
+        if gaps[0] <= 0:
+            continue
+        gaps[1] = buildings[i] - buildings[i-1]
+        if gaps[1] <= 0:
+            continue
+        gaps[2] = buildings[i] - buildings[i+1]
+        if gaps[2] <= 0:
+            continue
+        gaps[3] = buildings[i] - buildings[i+2]
+        if gaps[3] <= 0:
+            continue
+
+        min_gap = gaps[0]
+        for gap in gaps:
+            if gap < min_gap:
+                min_gap = gap
+            else:
+                pass
+
+        result += min_gap
+    print('#{} {}'.format(tc, result))

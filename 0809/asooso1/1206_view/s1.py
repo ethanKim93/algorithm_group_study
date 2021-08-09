efe06752d0effa1467782dd7680765b408f78db1
asooso12 <asooso1212@gmail.com>
2021-08-09 18:09:40 +0900

    1206 시간단축

--- 0809/asooso1/1206_view/s1.py
+++ 0809/asooso1/1206_view/s1.py
@@ -6,8 +6,10 @@ for tc in range(1, 11):
     L = int(input())
     buildings = list(map(int, input().split()))
     cnt = 0
-    for i in range(2, L - 2):
-        if buildings[i - 1] < buildings[i] and buildings[i - 2] < buildings[i] and buildings[i + 1] < buildings[i] and buildings[i + 2] < buildings[i] :
+    i = 2
+    while i < L - 2:
+        if buildings[i - 1] < buildings[i] and buildings[i - 2] < buildings[i] and buildings[i + 1] < buildings[i] and \
+                buildings[i + 2] < buildings[i]:
             list_gap = []
             list_gap.append(buildings[i] - buildings[i - 2])
             list_gap.append(buildings[i] - buildings[i - 1])
@@ -18,4 +20,20 @@ for tc in range(1, 11):
                 if gap < gap_min:
                     gap_min = gap
             cnt += gap_min
+            i += 3
+        else:
+            i += 1
+    # for i in range(2, L - 2):
+    #     if buildings[i - 1] < buildings[i] and buildings[i - 2] < buildings[i] and buildings[i + 1] < buildings[i] and buildings[i + 2] < buildings[i] :
+    #         list_gap = []
+    #         list_gap.append(buildings[i] - buildings[i - 2])
+    #         list_gap.append(buildings[i] - buildings[i - 1])
+    #         list_gap.append(buildings[i] - buildings[i + 1])
+    #         list_gap.append(buildings[i] - buildings[i + 2])
+    #         gap_min = list_gap[0]
+    #         for gap in list_gap:
+    #             if gap < gap_min:
+    #                 gap_min = gap
+    #         cnt += gap_min
+
     print('#{} {}'.format(tc, cnt))

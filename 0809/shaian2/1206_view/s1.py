0803afd08590bc58636156aacd3c17fab354c07e
underwater <shaian2@naver.com>
2021-08-09 17:55:44 +0900

    homework_view 제출1

--- 0809/shaian2/1206_view/s1.py
+++ 0809/shaian2/1206_view/s1.py
@@ -4,5 +4,25 @@ sys.stdin = open('input.txt')
 for tc in range(1, 11):
     L = int(input())
     buildings = list(map(int, input().split()))
-    print(L)
-    print(buildings)
+    result = 0
+    for i in range(2, L-2):
+        left = 0
+        right = 0
+        if buildings[i] > buildings[i-1] and buildings[i] > buildings[i-2]:
+            if buildings[i-1] > buildings[i-2]:
+                left = buildings[i] - buildings[i-1]
+            else:
+                left = buildings[i] - buildings[i-2]
+        if buildings[i] > buildings[i+1] and buildings[i] > buildings[i+2]:
+            if buildings[i+1] > buildings[i+2]:
+                right = buildings[i] - buildings[i+1]
+            else:
+                right = buildings[i] - buildings[i+2]
+        if left != 0 and right != 0:
+            if left > right:
+                result += right
+            else:
+                result += left
+    print('#{} {}'.format(tc, result))
+
+

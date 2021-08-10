f30ab831e62062d6cf83078ac8cba8e2d0dc31bb
jungmyeongsu <sdfg3034@naver.com>
2021-08-10 11:11:20 +0900

    정명수

---
+++ 0810/sdfg3034/4831_전기버스/s1.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+T=int(input())
+for i in range(1,T+1):
+    K, N, M=list(map(int, input().split()))
+    M_numbers = list(map(int, input().split()))
+    
+

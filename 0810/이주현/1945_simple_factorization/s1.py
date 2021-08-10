42b14012995067a70820dee95995837f42f2afc9
Ju Hyeon <aganga7427@gmail.com>
2021-08-10 13:08:42 +0900

    Change directory name

---
+++ 0810/이주현/1945_simple_factorization/s1.py
@@ -0,0 +1,16 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+
+    numbers = [2, 3, 5, 7, 11]
+    result = []
+
+    for number in numbers:
+        count = 0
+        while (N % number) == 0:
+            N //= number
+            count += 1
+        result.append(count)
+
+    print("#{}".format(test_case), end=" ")
+    print(*result)

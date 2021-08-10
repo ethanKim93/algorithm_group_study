02b5d6d17ecf01ff77b099220c10f874991dabcc
onehousesilver <onehousesilver@naver.com>
2021-08-10 17:12:27 +0900

    4828/4831/4834/4835

--- 0810/한채은/4835_구간합/s1.py
+++ 0810/한채은/4835_구간합/s1.py
@@ -6,21 +6,18 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     L = list(map(int, input().split()))
 
-    if N < M:
-        N, M = M, N
-
     maxi = 0
     mini = 999999
 
-    for i in range(N-M+1):
+    for i in range(N-M+1):  # M개의 합 비교 횟수만큼 (N-M+1)반복
         total = 0
-        for j in range(i, M+i):
+        for j in range(i, M+i): # M개의 합을 구하기
             total = total + L[j]
 
-        if total > maxi:
+        if total > maxi:    # max값 구하기
             maxi = total
 
-        if total < mini:
+        if total < mini:    # min값 구하기
             mini = total
 
     print('#{} {}'.format(tc, maxi - mini))

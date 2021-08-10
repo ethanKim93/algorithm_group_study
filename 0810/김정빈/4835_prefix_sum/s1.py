820d73551efea1c65ee03f144a019d5d520a7c99
jpNew-man <jaengpin@gmail.com>
2021-08-10 20:06:51 +0900

    과제제출

--- 0810/김정빈/4835_prefix_sum/s1.py
+++ 0810/김정빈/4835_prefix_sum/s1.py
@@ -7,19 +7,19 @@ for tc in range(1, T+1):
     N, M = map(int,input().split())
     ai = list(map(int,input().split()))
 
+    maxfix = -999999
+    minfix = 999999
+
     for i in range(N-M+1):
         prefix = ai[i:M+i]
         totfix = 0 #각 prefix의 합
 
-        maxfix = prefix[0]
-        minfix = prefix[0]
-
         for tot in prefix:
             totfix += tot
 
-    if totfix > maxfix:
-        maxfix = totfix
-    if totfix < minfix:
-        minfix = totfix
+        if totfix > maxfix:
+            maxfix = totfix
+        if totfix < minfix:
+            minfix = totfix
 
     print('#{} {}'.format(tc, maxfix-minfix))

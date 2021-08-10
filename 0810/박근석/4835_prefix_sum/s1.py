d6584772897e1db60bbd7717973ed4e4baa731e6
pgs3858 <pgs3858@naver.com>
2021-08-10 21:11:52 +0900

    0810제출

--- 0810/박근석/4835_prefix_sum/s1.py
+++ 0810/박근석/4835_prefix_sum/s1.py
@@ -6,22 +6,27 @@ T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     nums = list(map(int, input().split()))
-
     total = 0
-    min_total = 0
 
     for i in range(0, M):
         total += nums[i]
 
-    for i in range(0, M):
-        min_total += nums[i]
-    # print(min_total)
+    list_a = []
+    list_a.append(total)
 
     for i in range(0, N-M):
-        if total < total - nums[i] + nums[i+M]:
-            total = total - nums[i] + nums[i+M]
-        if min_total > min_total - nums[i] + nums[i+M]:
-            min_total = min_total - nums[i] + nums[i+M]
+        list_a.append(total - nums[i] + nums[i+M])
+        total = total - nums[i] + nums[i + M]
 
-    print(total)
-    print(min_total)
+    max_num = list_a[0]
+    for i in list_a:
+        if max_num < i:
+            max_num = i
+
+    min_num = list_a[0]
+    for j in list_a:
+        if min_num > j:
+            min_num = j
+
+    print('#{} {}'.format(tc, max_num - min_num))
+

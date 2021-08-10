a449f25512dfcc8ea73af3ebca0bd279bb001553
dbstnr2146 <dbstnr2146@naver.com>
2021-08-10 10:38:41 +0900

    0810__2

--- 0810/윤숙/4834_숫자카드/s1.py
+++ 0810/윤숙/4834_숫자카드/s1.py
@@ -2,14 +2,27 @@
 
 T=int(input())
 
-count=[]*10
+
 for tc in range(1,T+1):
+    count = [0] * 10
     n=int(input())
     card_number=list(map(int, input()))
 
     for i in card_number:
         count[i]+=1
+    arr=[]
+    max=0
+    for n in count:
+        if max<n:
+            max=n
+    for n in range(len(count)):
+        if max==count[n]:
+            arr.append(n)
+    arr_max=0
+    for n in arr:
+        if arr_max<n:
+            arr_max=n
 
 
+    print('#{} {}'.format(arr_max, count[arr_max]))
 
-    print(count)

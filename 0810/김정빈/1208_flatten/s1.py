120d797961ab38878234b17b0695bad4a8e3fc34
jpNew-man <jaengpin@gmail.com>
2021-08-10 23:11:46 +0900

    ㅠㅠㅠㅠflatten 해결

--- 0810/김정빈/1208_flatten/s1.py
+++ 0810/김정빈/1208_flatten/s1.py
@@ -2,12 +2,24 @@ import sys
 sys.stdin = open('input.txt')
 
 for T in range(1,11):
-    N = int(input()) #덤프 횟수
-    dump = list(map(int,input().split()))
+    dump = int(input()) #덤프 횟수
+    height = list(map(int,input().split())) #박스 높이
 
-    # for i in range(N):
-    #     dump[dump.index(max(dump))] -= 1
-    #     dump[dump.index(min(dump))] += 1
-    #
-    #
-    # print('#{} {}'.format(T, max(dump) - min(dump)))
+    idx_max = 0
+    idx_min = 0
+
+    for i in range(dump):
+        Min, Max = 99999999999999, -1
+        for j in range(0, 100):
+            if height[j] < Min:
+                Min = height[j]
+                idx_min = height.index(Min)
+            if height[j] > Max:
+                Max = height[j]
+                idx_max = height.index(Max)
+
+        if height[idx_max] - height[idx_min] > 1:
+            height[idx_max] = Max-1
+            height[idx_min] = Min+1
+
+    print('#{} {}'.format(T, max(height) - min(height)))

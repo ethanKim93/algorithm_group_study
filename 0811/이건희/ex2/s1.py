e9e4517fb22a56618d642245310f25408cd01604
gkuer <gkuer95@gmail.com>
2021-08-11 17:33:35 +0900

    submit_~Q3

--- 0811/이건희/ex2/s1.py
+++ 0811/이건희/ex2/s1.py
@@ -3,6 +3,8 @@ sys.stdin = open("input.txt")
 
 def check(num_list):
     subsets = []
+
+    # Bit mask > make subsets
     for i in range(1 << 10):
         temps = []
         for x in range(10):
@@ -11,15 +13,22 @@ def check(num_list):
         subsets.append(temps)
 
     for i in subsets:
-        sm = 0
-        for x in i:
-            sm += x
-        if sm == 0:
-            return 1
-        return 0
+        if i == []: # 빈리스트 예외처리
+            continue
+
+        else:
+            sm = 0
+            for x in i: # =sum(i)
+                sm += x
+
+            if sm == 0:
+                return 1
+
+    return 0
 
 t = int(input())
 for tc in range(1, t+1):
     num_list = list(map(int,input().split()))
 
+    # Print
     print("#{} {}".format(tc, check(num_list)))

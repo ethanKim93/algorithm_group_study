3fb3a293d7b16afd823c9ba38ef4778860ccf746
sdh98429 <sdh98429@naver.com>
2021-08-11 17:24:31 +0900

    0811 hw 제외하고 제출

---
+++ 0811/신동호/ex1/s1.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    box = [0] * N
+    new_box = [[0] * N for _ in range(N)]
+    for i in range(N):
+        box[i] = list(map(int,input().split()))
+    for j in range(N):
+        for i in range(N):
+            if 0 <= i-1:
+                new_box[i][j] += abs(box[i][j] - box[i-1][j])
+            if 0 <= j-1:
+                new_box[i][j] += abs(box[i][j] - box[i][j-1])
+            if N > i+1:
+                new_box[i][j] += abs(box[i][j] - box[i+1][j])
+            if N > j+1:
+                new_box[i][j] += abs(box[i][j] - box[i][j+1])
+    # print(box)
+    # print(new_box)
+    print('#{} {}'.format(tc,sum(sum(new_box,[]))))
+
---
+++ 0811/신동호/ex2/s1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    numbers = list(map(int,input().split()))
+    n = len(numbers)
+    for i in range(1<<n):
+        sub = []
+        for j in range(n):
+            if i & (1<<j) :
+                sub.append(numbers[j])
+        if len(sub) and (sum(sub) == 0):
+            print('#{} {}'.format(tc,1))
+            break
+    else:
+        print('#{} {}'.format(tc,0))

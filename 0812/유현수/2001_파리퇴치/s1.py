699bf08f1751f130793083336469843c215484f6
82surf <82surf@gmail.com>
2021-08-12 12:47:46 +0900

    2001 문제풀이 완료

---
+++ 0812/유현수/2001_파리퇴치/s1.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin=open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for i in range(N)]
+
+    kills = []
+    for i in range(0, N-M+1):
+        for j in range(0, N-M+1):
+            kill = 0
+            for k in range(0+i, M+i):
+                for l in range(0+j, M+j):
+                    kill += arr[k][l]
+            kills += [kill]
+
+    max_kill = kills[0]
+    for k in kills:
+        if max_kill < k:
+            max_kill = k
+
+    print('#{} {}'.format(tc, max_kill))

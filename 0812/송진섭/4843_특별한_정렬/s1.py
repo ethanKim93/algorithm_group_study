88d1866a5a4258acff36575efd4de61a5d561511
Songjoey12 <songjoey1205@gmail.com>
2021-08-12 17:45:16 +0900

    Submit 4843 and 2001

--- 0812/송진섭/4843_특별한_정렬/s1.py
+++ 0812/송진섭/4843_특별한_정렬/s1.py
@@ -1,19 +1,26 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
-def selection_sort(arr):
-    for i in range(len(arr)-1):
-        min_i = i
-        for j in range(i+1, len(arr)):
-            if arr[min_i] > arr[j]:
-                min_i = j
-            arr[i], arr[min_i] = arr[min_i], arr[i]
-    return arr
+
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = list(map(int, input().split()))
-    print(arr)
-    print(selection_sort(arr))
-    for i in range(1, len(arr)//2):
+
+    for i in range(10):
+        if not i % 2: # 짝수 인덱스일경우 최대값
+            max_i = i
+            for j in range(i+1, len(arr)):
+                if arr[max_i] < arr[j]:
+                    max_i = j
+            arr[i], arr[max_i] = arr[max_i], arr[i]
+        else:  # 홀수 인덱스일 경우 최소값
+            min_i =i
+            for j in range(i+1, len(arr)):
+                if arr[min_i] > arr[j]:
+                    min_i = j
+            arr[i], arr[min_i] = arr[min_i], arr[i]
+    res_arr = arr[:10]
+
+    print('#{} {}'.format(tc, *res_arr))

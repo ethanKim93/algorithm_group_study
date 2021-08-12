4b2372c8551c1c9f20f80de46774f47239e993a2
asooso12 <asooso1212@gmail.com>
2021-08-12 15:21:29 +0900

    1966 코드수정

--- 0812/이진석/1966_숫자를_정렬하자/s1.py
+++ 0812/이진석/1966_숫자를_정렬하자/s1.py
@@ -3,26 +3,24 @@ import sys
 sys.stdin = open('input.txt')
 
 
-def quick_sort(arr):
+def selection_sort(arr):  # 선택정렬
     """
-    quick sort : 기준점을 잡고 그 수보다 큰 배열, 작은 배열을 나누는 함수를 재귀형태로 해서 정렬
+    선택정렬 : 최소값을 차례대로 선택하여 0부터 위치를 교환하는 정렬방식
     """
-    if len(arr) <= 1:  # 재귀 종료
-        return arr
-    pivot = arr[0]  # 기준점
-    prev_arr, next_arr = [], []
-    for num in arr[1:]: # 기준점 이후 인덱스의 값을 기준점보다 작은지 큰지 구분
-        if num < pivot:
-            prev_arr.append(num)
-        else:
-            next_arr.append(num)
-
-    return quick_sort(prev_arr) + [pivot] + quick_sort(next_arr) # 작은수의 배열, 기준점, 큰 수의 배열
+    for i in range(len(arr)):
+        min_idx = i
+        min_num = arr[i]
+        for j in range(i, len(arr)):
+            if min_num > arr[j]:
+                min_num = arr[j]
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
 
 
 for tc in range(1, int(input()) + 1):
     N = int(input())
     numbers = list(map(int, input().split()))
-    numbers = quick_sort(numbers)
+    numbers = selection_sort(numbers)
     answer = ' '.join(map(str, numbers))
     print('#{} {}'.format(tc, answer))

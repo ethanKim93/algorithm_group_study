0f4a8f2ed9b37cab8881977d10e3775645976204
82surf <82surf@gmail.com>
2021-08-12 12:50:43 +0900

    2001 주석 추가

--- 0812/유현수/2001_파리퇴치/s1.py
+++ 0812/유현수/2001_파리퇴치/s1.py
@@ -7,15 +7,15 @@ for tc in range(1, T+1):
     arr = [list(map(int, input().split())) for i in range(N)]
 
     kills = []
-    for i in range(0, N-M+1):
+    for i in range(0, N-M+1):               # MxM 영역 이동시키기
         for j in range(0, N-M+1):
             kill = 0
-            for k in range(0+i, M+i):
+            for k in range(0+i, M+i):       # MxM 영역의 숫자 합 구하기
                 for l in range(0+j, M+j):
                     kill += arr[k][l]
             kills += [kill]
 
-    max_kill = kills[0]
+    max_kill = kills[0]                     # 최댓값 구하기
     for k in kills:
         if max_kill < k:
             max_kill = k

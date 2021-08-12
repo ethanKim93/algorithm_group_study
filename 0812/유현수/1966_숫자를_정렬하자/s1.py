86c361d7b383d049aa8a5d04ee3ec8b522660b98
82surf <82surf@gmail.com>
2021-08-12 14:36:45 +0900

    1966 코드 수정

--- 0812/유현수/1966_숫자를_정렬하자/s1.py
+++ 0812/유현수/1966_숫자를_정렬하자/s1.py
@@ -6,9 +6,23 @@ for tc in range(1, T+1):
     N = int(input())
     arr = list(map(int, input().split()))
 
-    for i in range(N-1, 0, -1):     # 버블정렬
-        for j in range(0, i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    max_n = 0
+    for n in arr:
+        if max_n < n:
+            max_n = n
+
+    counts = [0] * (max_n + 1)
+
+    for i in range(N):
+        counts[arr[i]] += 1
+
+    for i in range(max_n):
+        counts[i+1] += counts[i]
+
+    result = [0] * N
+    for i in range(N):
+        result[counts[arr[i]]-1] = arr[i]
+        counts[arr[i]] -= 1
+
     print('#{} '.format(tc), end='')
-    print(*arr, sep=' ')
+    print(*result, sep=' ')

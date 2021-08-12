965e9a5ddd7069da6af0d15efdd5c4722e808182
Leo <guswhddlf11@gmail.com>
2021-08-12 17:37:51 +0900

    숫자정렬, 사다리 제외

--- 0812/현종일/4836_색칠하기/s1.py
+++ 0812/현종일/4836_색칠하기/s1.py
@@ -5,20 +5,21 @@ T = int(input())
 
 for tc in range(1,T+1):
     N = int(input())
-    arr = [[0]*10 for _ in range(10)]
-    result = 0
+    arr = [[0]*10 for _ in range(10)] # 10*10 빈배열
+    result = 0 # 보라색 갯수를 담을 result
     for i in range(N):
         area = list(map(int, input().split()))
-        x = [area[0], area[2]]
-        y = [area[1], area[3]]
-        for j in range(x[0], x[1]+1):
-            for k in range(y[0], y[1]+1):
-                if not arr[j][k]:
-                    arr[j][k] = area[4]
+        x = [area[0], area[2]]      # x축 점 두개
+        y = [area[1], area[3]]      # y축 점 두개
+
+        for j in range(x[0], x[1]+1):    # x축 점 두개 사이 반복
+            for k in range(y[0], y[1]+1):# y축 점 두개 사이 반복
+                if not arr[j][k]:        # 안의 값이 0이 아니라면
+                    arr[j][k] = area[4]  # 1,2 둘중 하나 대입
                 else:
                     arr[j][k] = 3
 
-    for l in range(10):
+    for l in range(10):         # 이중배열내에서 보라색값 구하기
         for m in range(10):
             if arr[l][m] == 3:
                 result += 1
--- 0812/현종일/4837_부분집합의_합/s1.py
+++ 0812/현종일/4837_부분집합의_합/s1.py
@@ -5,12 +5,15 @@ T = int(input())
 
 for tc in range(1, T+1):
     N, K = map(int, input().split())
-    arr = []
-    for idx in range(1,N+1):
-        arr.append(idx)
-
-    for i in range(1, 1<<N):
-        for j in range(N):
-            if i & (1<<j):
-                print(arr[j], end="")
-        print()
+    arr = list(range(1, 13))
+    total = 0
+    for i in range(1 << 12):
+        numsum = 0
+        cnt = 0
+        for j in range(12):
+            if i & (1 << j):
+                numsum += arr[j]
+                cnt += 1
+        if numsum == K and N == cnt:
+            total += 1
+    print('#{} {}'.format(tc, total))

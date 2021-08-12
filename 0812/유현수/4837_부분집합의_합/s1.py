b11fbb6ed2854085c5957b8f1c8979bb57aca943
82surf <82surf@gmail.com>
2021-08-12 13:12:10 +0900

    4837 코드 수정

--- 0812/유현수/4837_부분집합의_합/s1.py
+++ 0812/유현수/4837_부분집합의_합/s1.py
@@ -4,7 +4,7 @@ sys.stdin=open('sample_input.txt')
 T = int(input())
 for tc in range(1, T+1):
     N, K = map(int, input().split())
-    arr = list(range(1, 13))
+    arr = range(1, 13)
 
     result = 0
     for i in range(1<<12):              # arr의 부분집합

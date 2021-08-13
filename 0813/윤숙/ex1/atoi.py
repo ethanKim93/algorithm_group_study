e187095b1d7e77a5491a41b20dd451efd2f9f595
dbstnr2146 <dbstnr2146@naver.com>
2021-08-13 16:42:06 +0900

    ex2진행중

--- 0813/윤숙/ex1/atoi.py
+++ 0813/윤숙/ex1/atoi.py
@@ -1,14 +1,31 @@
-#정수를 문자열로 변환
-find_num=''
-def atoi(num):
 
-    str_num=['0','1','2','3','4','5','6','7','8',',9']
+#0은 48
+#문자열을 정수로 변환
+def atoi(str_num):
+    N_t=int(str_num)
+    arr_num=[]
+    while True:
+        N_d=N_t%10
+        for i in range(48,58):
+            if N_d==str(i):
+                arr_num.append(str(i))
 
-    find_num=str_num[num]
 
-    return find_num
+        N_t = N_t/10
 
 
-result=atoi(1)
+    int_num="".join(arr_num)
+
+    return int(int_num)
+
+
+
+
+# 정수를 문자열로 변환
+# def itoa(num):
+
+
+
+result=atoi('1245')
 
 print(result,type(result))
--- 0813/윤숙/ex1/s1.py
+++ 0813/윤숙/ex1/s1.py
@@ -1,3 +1,39 @@
-s='Reverse this strings'
+s = 'Reverse this strings'
 
-print(s[::-1])
+# 1. slice() 사용
+
+# print(s[::-1])
+
+
+# 2. reverse() 사용
+
+# List_S=[]
+# for j in s:
+#     List_S.append(j)
+#
+# List_S.reverse()
+#
+# result=','.join(List_S)
+#
+# print(result)
+
+
+# 3. List를 이용한 방법
+List_S=[]
+for j in s:
+    List_S.append(j)
+
+N=len(List_S)
+N=N-1
+idx=int(len(List_S)/2)
+                #10까지
+
+for i in range(0,idx,1):
+    tmpA=List_S[i] #0 #1
+    tmpB=List_S[N-i] #9 #8
+    List_S[i]=tmpB
+    List_S[N-i]=tmpA
+
+result=''.join(List_S)
+
+print(result)

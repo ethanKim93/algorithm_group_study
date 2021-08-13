d75069449d2c89c86f543891b6b765631802ac80
82surf <82surf@gmail.com>
2021-08-13 13:17:13 +0900

    연습문제 풀이 완료

---
+++ 0813/유현수/ex2/s1.py
@@ -0,0 +1,18 @@
+# str() 없이 itoa() 구현
+
+
+def my_itoa(n):
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    result = ''
+    while n > 0:
+        temp = n % 10
+        for i in range(10):
+            if i == temp:
+                result = nums[i] + result
+                break
+        n -= temp
+        n /= 10
+    return result
+
+
+print(my_itoa(123))

0694e38918ee818b72291ec1da74d75164053bad
안재현 <sevct@naver.com>
2021-08-13 13:24:57 +0900

    function_0813

--- 0813/안재현/ex1.py
+++ 0813/안재현/ex1.py
@@ -1,6 +1,13 @@
-a = '12345'
-b = ''
-List_a = list(a)
-for i in range(len(a)):
-    b += List_a[len(a) - i - 1]
-print(b)
+s = '12345'
+
+
+def reverse(a):
+    b = ''
+    List_a = list(a)
+    for i in range(len(a)):
+        b += List_a[len(a) - i - 1]
+    return b
+
+
+print(s)
+print(reverse(s))

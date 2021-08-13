ca197dde0443a7816e8e306f989f7e8f50873b41
eora21 <eora21@naver.com>
2021-08-13 13:24:12 +0900

    atoi and itoa

--- 0813/김주호/ex2/s1.py
+++ 0813/김주호/ex2/s1.py
@@ -1,4 +1,4 @@
-def itoa(string):
+def atoi(string):
     total = 0
     for i in range(len(string)):
         total *= 10
@@ -6,4 +6,17 @@ def itoa(string):
     return total
 
 
-print(itoa("1234567890"))
+def itoa(nums, string):
+    li = []
+    while nums > 0:
+        num = nums % 10
+        li.insert(0, chr(num + 48))
+        nums = nums // 10
+    string = "".join(li)
+    print(string)
+
+
+print(atoi("1234567890"))
+
+st = ""
+itoa(1234567890, st)

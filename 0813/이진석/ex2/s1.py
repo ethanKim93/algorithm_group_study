1689d96f2c071184200f2af0fe992eec29cb41f7
asooso12 <asooso1212@gmail.com>
2021-08-13 13:52:42 +0900

    ex2 수정

--- 0813/이진석/ex2/s1.py
+++ 0813/이진석/ex2/s1.py
@@ -1,4 +1,4 @@
-def itoa(number, string):
+def itoa(number):
     list_temp = []
     if number < 0:  # 음수일때
         number *= (-1)
@@ -11,15 +11,24 @@ def itoa(number, string):
             list_temp.insert(0, chr(number % 10 + 48))
             number //= 10
     string = ''.join(list_temp)
-    print(string)
+    return string
 
 
 def atoi(string):
     number = 0
-    for char in string:
+    minus = 0
+    if string[0] == '-':  # 음수인경우 확인
+        minus = 1
+        string = string[1:]
+    for char in string:  # 0번인덱스(가장 큰 자리수) 부터 ascii 변환을 하면서 숫자로 바꿔주고 10을 곱해준다
         number *= 10
         number += ord(char) - 48
-    print(number)
+    if minus:  # 음수일 경우 -1을 곱해준다
+        number *= (-1)
+    return number
 
-itoa(123, '')
-atoi('1234')
+
+print(itoa(123), type(itoa(123)))
+print(itoa(-123), type(itoa(123)))
+print(atoi('1234'), type(atoi('1234')))
+print(atoi('-1234'), type(atoi('1234')))

6f6edd7020d0bc28a81b1f47208dd5de8b0cdf9a
82surf <82surf@gmail.com>
2021-08-13 13:51:16 +0900

    ex2 itoa 수정

--- 0813/유현수/ex2/s1.py
+++ 0813/유현수/ex2/s1.py
@@ -3,29 +3,37 @@
 
 def my_itoa(n):
     result = ''
-    while n > 0:
-        temp = n % 10
-        s_temp = chr(temp + 48)
-        result = s_temp + result
-        n -= n % 10
-        n //= 10
-    return result
+    if n >= 0:
+        while n > 0:
+            temp = n % 10
+            s_temp = chr(temp + 48)
+            result = s_temp + result
+            n -= n % 10
+            n //= 10
+        return result
+    else:
+        n = -n
+        while n > 0:
+            temp = n % 10
+            s_temp = chr(temp + 48)
+            result = s_temp + result
+            n -= n % 10
+            n //= 10
+        result = '-' + result
+        return result
 
 
-print(my_itoa(123))
+print(my_itoa(-123))
 
 
 def my_atoi(s):
     result = 0
-
     s_len = 0
     for _ in s:
         s_len += 1
-
     for i in range(s_len):
         temp = ord(s[i]) - 48
         result += temp * (10 ** (s_len - i - 1))
-
     return result
 
 

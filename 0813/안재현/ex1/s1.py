b1f43e6b63313ed4720a40444b7f4cd6e4457881
안재현 <sevct@naver.com>
2021-08-13 13:36:12 +0900

    little+

--- 0813/안재현/ex1/s1.py
+++ 0813/안재현/ex1/s1.py
@@ -1,7 +1,14 @@
-s = '12345'
+k = '12345'
 
 
-def reverse(a):
+def reverse1(a):
+    s = list(a)
+    n = len(s)
+    for i in range(n//2):
+        s[i], s[n-1-1] = s[n-1-i], s[i]
+    return ''.join(map(str, s))
+
+def reverse2(a):
     b = ''
     List_a = list(a)
     for i in range(len(a)):
@@ -9,5 +16,6 @@ def reverse(a):
     return b
 
 
-print(s)
-print(reverse(s))
+print(k)
+print(reverse1(k))
+print(reverse2(k))
--- 0813/안재현/ex2/s1.py
+++ 0813/안재현/ex2/s1.py
@@ -2,7 +2,7 @@ def change_str(x):
     return '{}'.format(x)
 
 
-a = '12345'
+a = 12345
 neg = -1
 print(change_str(a))
 print(type(change_str(a)))

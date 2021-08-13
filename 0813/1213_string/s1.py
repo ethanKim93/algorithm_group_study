4e3def1cfeef2077e59b1673554d48c200b57a45
YUNYEOCHANG <ron2003@naver.com>
2021-08-13 17:25:22 +0900

    제출

--- 0813/1213_string/s1.py
+++ 0813/1213_string/s1.py
@@ -7,8 +7,8 @@ for i in range(10):
     sentence = input()
 
     cnt = 0
-    for i in range(len(senten) - len(patt) + 1):
-        if senten[i:i + len(patt)] == patt:
+    for i in range(len(sentence) - len(patt) + 1):
+        if sentence[i:i + len(patt)] == patt:
             cnt += 1
 
     print('#{} {}'.format(t, cnt))

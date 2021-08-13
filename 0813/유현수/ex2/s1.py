af3fed4c55c83b6fd4a24aa83721b31c7b39013b
82surf <82surf@gmail.com>
2021-08-13 13:40:02 +0900

    ex1 수정

--- 0813/유현수/ex2/s1.py
+++ 0813/유현수/ex2/s1.py
@@ -2,17 +2,14 @@
 
 
 def my_itoa(n):
-    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     result = ''
     while n > 0:
         temp = n % 10
-        for i in range(10):
-            if i == temp:
-                result = nums[i] + result
-                break
-        n -= temp
-        n /= 10
+        s_temp = chr(temp + 48)
+        result = s_temp + result
+        n -= n % 10
+        n //= 10
     return result
 
 
-print(my_itoa(123))
+print(my_itoa(100))

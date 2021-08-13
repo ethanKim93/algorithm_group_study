de1a2f76e0473cfc7b1c0a318f0b585719d980a6
onehousesilver <onehousesilver@naver.com>
2021-08-13 22:49:46 +0900

    제출합니다

--- 0813/한채은/ex2/s1.py
+++ 0813/한채은/ex2/s1.py
@@ -1,8 +1,16 @@
-def itoa(nums,s):
+def itoa(nums):
+    st = ''
+    if nums < 0 :
+        nums *= -1
+        st += "-"
+
     llist = []
-    while n > 0 :
+    while nums > 0 :
         num = nums % 10 # 맨 뒤에서부터
         llist.insert(0, chr(num + 48)) # 추출하고 리스트에 담아주기
         nums = int(nums//10)
-    s = "".join(llist)
-    print(s)
+
+    for i in range(len(llist)):
+        st += llist[i]
+
+    return st

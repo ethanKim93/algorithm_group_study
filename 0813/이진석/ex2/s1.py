9f6d626de3815f070e32d4c1c936f75d6880842a
asooso12 <asooso1212@gmail.com>
2021-08-13 13:38:54 +0900

    0813 practice

--- 0813/이진석/ex2/s1.py
+++ 0813/이진석/ex2/s1.py
@@ -1,24 +1,17 @@
 num = int(input())
 str_num = ''
 
-
 def itoa(number, string):
     list_temp = []
-    if number < 0:
+    if number < 0:  # 음수일때
         number *= (-1)
         while number:
-            temp = number % 10
-            char = chr(temp + 48)
-
-            list_temp.insert(0, char)
+            list_temp.insert(0, chr(number % 10 + 48))
             number //= 10
         list_temp.insert(0, '-')
-    else:
+    else:   # 양수일때
         while number:
-            temp = number % 10
-            char = chr(temp + 48)
-
-            list_temp.insert(0, char)
+            list_temp.insert(0, chr(number % 10 + 48))
             number //= 10
     string = ''.join(list_temp)
     print(string)

f92e790ba94bccd37510e8e2490e58484db9ef97
안재현 <sevct@naver.com>
2021-08-17 13:04:38 +0900

    annotation_0817

--- 0817/안재현/4864_문자열_비교/s1.py
+++ 0817/안재현/4864_문자열_비교/s1.py
@@ -7,16 +7,16 @@ for tc in range(0, T):
     R = input()
     A = input()
     C = ''
-    count = 0
-    search = 0
+    count = 0   # M 문자열이 N 문자열 일부와 동일한지 체크
+    search = 0  # while문을 돌리기 위한 값
     while search + len(R) <= len(A):
         for i in range(len(R)):
-            C += A[i + search]
+            C += A[i + search]  # C에 R의 길이만큼 A를 잘라서 넣는다.
         if C == R:
-            count = 1
+            count = 1           # 동일할 경우 count를 1로 변경
             search += 1
             C = ''
         else:
-            search += 1
+            search += 1         # 동일하지 않을 경우 그냥 넘긴다.
             C = ''
     print('#{} {}'.format(tc+1, count))
--- 0817/안재현/4865_글자수/s1.py
+++ 0817/안재현/4865_글자수/s1.py
@@ -3,22 +3,20 @@ import sys
 sys.stdin = open('sample_input.txt', 'rt', encoding='UTF8')
 
 T = int(input())
-dic = {}
-result = 0
+
 for i in range(0, T):
     N = input()
     M = input()
-
-    for j in N:
-        dic[j] = 0
-
-    for key, values in dic.items():
-        for a in range(0, len(M)):
-            if key == M[a]:
-                dic[key] += 1
-                for val in dic.values():
-                    if dic[key] > result:
-                        result = dic[key]
-    print('#{} {}'.format(i+1, result))
-    dic = {}
+    my_dict = {}
     result = 0
+    for j in N:     # N 문자열을 글자별로 나누어 딕셔너리에 담는다
+        my_dict[j] = 0
+
+    for key, values in my_dict.items():
+        for a in range(0, len(M)):  # M 문자열의 글자를 1개씩 my_dict의 key값과 비교한다.
+            if key == M[a]:   # 동일할 경우 해당 key의 value값에 1을 더한다.
+                my_dict[key] += 1
+                for val in my_dict.values():    # value값 중 최댓값을 result에 저장.
+                    if my_dict[key] > result:
+                        result = my_dict[key]
+    print('#{} {}'.format(i+1, result))

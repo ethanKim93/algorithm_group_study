c919ddfe808d56028a75f85f6dbb9da93b7d6d1a
onehousesilver <onehousesilver@naver.com>
2021-08-17 16:01:30 +0900

    제출합니다

---
+++ 0817/한채은/4864_문자열_비교/s1.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("sample_input.txt")
+
+T = int(input())
+for tc in range(1, T+1):
+    str1 = input()
+    str2 = input()
+
+    result = 0
+
+    for i in range(len(str2)-len(str1)+1):
+        if str2[i:1+len(str1)] == str1:
+            result = 1
+
+    print('#{} {}'.format(tc, result))

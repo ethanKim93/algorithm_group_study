42275a0e165b032085ce502f1f67cc7c6ea8d10c
Sagytic <jb9992@daum.net>
2021-08-17 16:06:08 +0900

    쇠막대기중

--- 0817/장지빈/4861_회문/s1.py
+++ 0817/장지빈/4861_회문/s1.py
@@ -6,16 +6,16 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     answer = []
     ls = []
-
+    # N, M input 받을 변수 선언
+    # 열우선 순회를 위한 ls 리스트 선언
     for n in range(N):
         row = input()
-        ls.append(row)
+        ls.append(row)  #입력받은 문자열을 열우선 순회를 하기 위해 ls 리스트에 저장
         for a in range(N-M+1):
             if row[a:M+a] == row[a:M+a][::-1]:
                 answer.append(row[a:M+a])
-            elif row[0:M+1] == row[0:M+1][::-1]:
-                answer.append(row)
-
+    # 가로 문자열의 앞 뒤를 바꾸었을 때 서로 일치한다면 answer에 해당 문자열을 추가
+    # 문자열의 길이가 앞에서 뒤 끝까지가 아닌 경우 0번 인덱스부터 M번 인덱스까지 반복문을 통해 검사
     for i in range(N):
         col = []
         joincol = ''
@@ -24,10 +24,9 @@ for tc in range(1, T+1):
         joincol = ''.join(col)
         if joincol == joincol[::-1]:
             answer.append(joincol)
-            break
-        for a in range(N-M+1):
-            if joincol[a:M+a] == joincol[a:M+a][::-1]:
-                answer.append((joincol[a:M+a]))
+    for a in range(N-M+1):
+        if joincol[a:M+a] == joincol[a:M+a][::-1]:
+            answer.append((joincol[a:M+a]))
 
     answer = ''.join(answer)
     print('#{} {}'.format(tc, answer))

90f976a6b9a979edc7a043a8d052207205bf0432
Felix0708 <chuls0708@gmail.com>
2021-08-17 21:54:32 +0900

    0817 hw

--- 0817/이동철/3143_가장_빠른_문자열/s1.py
+++ 0817/이동철/3143_가장_빠른_문자열/s1.py
@@ -10,15 +10,11 @@ for tc in range(1, T+1):
     while True:
         if len(A[idx:]) < len(B):
             break
-    # for idx in range(0, len(A)-len(B)+1):
-    #     if len(A[idx:]) < len(B):
-    #         break
         if A[idx:idx+len(B)] == B:
             cnt += 1
             idx += len(B)
         else:
             idx += 1
-    # print(cnt)
 
     if cnt == 0:
         print('#{} {}'.format(tc, len(A)))

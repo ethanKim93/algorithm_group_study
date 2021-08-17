6cfff66d36c234d9447ca8781706e162e54d7aad
rkdms0116 <rkdms0116@gmail.com>
2021-08-18 00:24:06 +0900

    쇠막대기 해결했습니다! :)

--- 0817/이가은/5432_쇠막대기_자르기/s1.py
+++ 0817/이가은/5432_쇠막대기_자르기/s1.py
@@ -2,8 +2,21 @@ import sys
 sys.stdin = open("sample_input.txt")
 
 T = int(input())
-for test in range(T)
-    put = input()
+for test in range(T):
+    pipe = str(input())
 
-    for p in range(put):
-        
+    stack = 0           # 쌓인 pipe 개수
+    cnt = 0             # 잘린 pipe 개수
+
+    pipe = pipe.replace('()', 'c', pipe.count('()')) # pipe에서 레이저'()'가 나오면 c로 대치
+
+    for p in pipe:
+        if p == '(':
+            stack += 1
+        elif p == ')':
+            cnt += 1
+            stack -= 1
+        else:
+            cnt += stack
+
+    print('#{} {}'.format(test+1, cnt))

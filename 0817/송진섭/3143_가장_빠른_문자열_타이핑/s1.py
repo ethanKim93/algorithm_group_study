9aded58f7cfb42a05f66e21a00ee708624a88ce1
Songjoey12 <songjoey1205@gmail.com>
2021-08-17 17:23:51 +0900

    Submit 3143

--- 0817/송진섭/3143_가장_빠른_문자열_타이핑/s1.py
+++ 0817/송진섭/3143_가장_빠른_문자열_타이핑/s1.py
@@ -1,2 +1,9 @@
 import sys
-sys.stdin = open('')
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    A, B = input().split()
+
+    new_A = A.replace(B, 'B')
+    print('#{} {}'.format(tc, len(new_A)))

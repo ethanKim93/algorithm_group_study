3730aa52e5b593ff992953c21b0dcd67efc21730
ghkdlxld <ohsg97@gmail.com>
2021-08-17 17:47:23 +0900

    8/17 homework_1216

--- 0817/오수경/1216_회문2/s1.py
+++ 0817/오수경/1216_회문2/s1.py
@@ -7,33 +7,31 @@ for tc in range(1, 11):
     T = input()
     board = [list(input()) for _ in range(100)]
     longest = []
-    for M in range(100, 2, -1): # 회문의 길이
-        for i in board:
-            a = 0
-            while a + M <= len(i):
-                if i[a:a+M] == i[a:a+M][-1::-1]:
-                    longest.append(M)
-                    break
 
-                else:
-                    a += 1
 
+    def is_pal(board):
+        for M in range(100, 2, -1):  # 회문의 길이
+            for i in board:
+                a = 0
+                while a + M <= len(i):
+                    if i[a:a + M] == i[a:a + M][-1::-1]:
+                        return longest.append(M)
+
+                    else:
+                        a += 1
+
+
+    is_pal(board)
     for i in range(100):
         for j in range(100):
             if i < j:
                 board[i][j], board[j][i] = board[j][i], board[i][j]
 
-    for M in range(100, 2, -1): # 회문의 길이
-        for i in board:
-            a = 0
-            while a + M <= len(i):
-                if i[a:a+M] == i[a:a+M][-1::-1]:
-                    longest.append(M)
-                    break
+    is_pal(board)
 
-                else:
-                    a += 1
+    big = 0
+    for long in longest:
+        if long > big:
+            big = long
 
-
-
-    print('#{} {}'.format(tc, max(longest)))
+    print('#{} {}'.format(tc, big))

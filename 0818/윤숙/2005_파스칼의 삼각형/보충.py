029796e99a894ad5c0ebabd13fe526b82b0c3010
dbstnr2146 <dbstnr2146@naver.com>
2021-08-18 19:07:43 +0900

    0818 과제진행중

---
+++ 0818/윤숙/2005_파스칼의 삼각형/보충.py
@@ -0,0 +1,24 @@
+# (a+b)=1
+# (a+b)^1=1a+1b
+# (a+b)^2=1a^2+2ab+1b^2
+# (a+b)3=1a^3+3a^2b+3ab^2+1b^3
+
+
+# j==0 ->1 i==j -> 1
+# p[i-1][j] , p[i-1][j-1], i>j
+p = [[0] * 10 for _ in range(10)]  # 문제조건에서 최대 10줄
+for i in range(10):
+    for j in range(i + 1):
+        if j == 0 or i == j:
+            p[i][j] = 1
+        else:
+            p[i][j] = p[i - 1][j - 1] + p[i - 1][j]
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    print('#{}'.format(tc))
+    for i in range(N):
+        for j in range(i+1): #대각선상의 원소까지
+            print(p[i][j], end=' ')
+        print()
---
+++ 0818/윤숙/ex1/s1.py
@@ -0,0 +1,52 @@
+
+
+
+def stackPush(arr,M):
+    global cnt
+    check=isOverflow()
+    if check==-1:
+        return print('overflow')
+
+    arr.insert(cnt,M)
+    cnt+=1
+
+
+def stackPop(arr):
+    global cnt
+    check=isUnderflow()
+    if check==-1:
+        return  print('underflow')
+
+    arr.pop(-1)
+    cnt -= 1
+
+def isOverflow():
+    global cnt
+    if cnt == n:
+        return -1
+    else:
+        return 0
+
+def isUnderflow():
+    global cnt
+    if cnt <= 0:
+        return -1
+    else:
+        return 0
+def CheckStack(arr):
+    print(arr)
+
+
+n=10
+cnt=0
+arr = []
+
+stackPush(arr,'A')
+stackPush(arr,'B')
+stackPush(arr,'A')
+CheckStack(arr)
+stackPop(arr)
+stackPop(arr)
+stackPop(arr)
+stackPop(arr)
+CheckStack(arr)

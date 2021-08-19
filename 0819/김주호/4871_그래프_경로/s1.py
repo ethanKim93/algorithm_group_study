15834f8bb8dbb21f0929cf75d5f7b42651c834b6
eora21 <eora21@naver.com>
2021-08-19 16:02:41 +0900

    0819

--- 0819/김주호/4871_그래프_경로/s1.py
+++ 0819/김주호/4871_그래프_경로/s1.py
@@ -4,7 +4,6 @@ sys.stdin = open("sample_input.txt")
 for case in range(int(input())):
     V, E = map(int, input().split())
     stack = []
-    place = [0] * V
     li = [[0] * V for _ in range(V)]
 
     for _ in range(E):
@@ -12,14 +11,13 @@ for case in range(int(input())):
         li[LS - 1][LE - 1] = 1
 
     S, G = map(int, input().split())
-    place[S - 1] = 1
     stack.append(S - 1)
 
     ans = 0
     while len(stack):
         for EP in range(V):
-            if li[stack[-1]][EP] and not place[EP]:
-                place[EP] = 1
+            if li[stack[-1]][EP]:
+                li[stack[-1]][EP] = 0
                 stack.append(EP)
                 if EP == G - 1:
                     ans = 1

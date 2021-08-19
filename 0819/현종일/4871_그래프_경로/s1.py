3b1bd2256454e8f5b42922ca42cab9bef1bde5dc
Leo <guswhddlf11@gmail.com>
2021-08-19 15:33:09 +0900

    그래프모르겠음

--- 0819/현종일/4871_그래프_경로/s1.py
+++ 0819/현종일/4871_그래프_경로/s1.py
@@ -10,9 +10,9 @@ for tc in range(1, T+1):
         n1, n2 = map(int, input().split())
         arrs.append([n1,n2])
     start, end = map(int, input().split())
-    route = []
-    for arr in arrs:
-        if arr[0] == start:
-            route.append(arr)
-        if arr[1] == end:
-            route.append(arr)
+#     route = []
+#     for arr in arrs:
+#         if arr[0] == start:
+#             route.append(arr)
+#         if arr[1] == end:
+#             route.append(arr)

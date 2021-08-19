20081ee323eb892e27de8abdb17b092fc28b763e
안재현 <sevct@naver.com>
2021-08-19 23:59:03 +0900

    little_error

--- 0819/안재현/1219_길찾기/s1.py
+++ 0819/안재현/1219_길찾기/s1.py
@@ -5,15 +5,15 @@ sys.stdin = open("input.txt")
 for a in range(0, 10):
     tc, n = map(int, input().split())
     num_list = list(map(int, input().split()))
-
     dic = {x: [] for x in range(100)}
+
     for i in range(0, n * 2, 2):
         my_key = num_list[i]
         my_val = num_list[i + 1]
         dic[my_key].append(my_val)
 
     stack = [0]
-    visited = stack * 100
+    visited = [0] * (100)
     visited[0] = 1
     answer = 0
 
@@ -27,4 +27,4 @@ for a in range(0, 10):
                 stack.append(j)
                 visited[j] = 1
 
-    print('#{} {}'.format(tc + 1, answer))
+    print('#{} {}'.format(tc, answer))

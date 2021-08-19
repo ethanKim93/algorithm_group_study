8153557408f848a46ae4783d93bccfc92c4fc4e0
sdh98429 <sdh98429@naver.com>
2021-08-19 23:22:52 +0900

    0819 hw까지

--- 0819/신동호/4871_그래프_경로/s1.py
+++ 0819/신동호/4871_그래프_경로/s1.py
@@ -5,6 +5,18 @@ T = int(input())
 
 for tc in range(1, T+1):
     V, E = list(map(int, input().split()))
-    matrix = [[0] * (V+1)] * (V+1)
-    [list(map(int, input().split())) for _ in range(E)]
-    print(matrix)
+    visited = [0] * (V+1)
+    matrix = [[] for _ in range(V+1)]
+    for _ in range(E):
+        st, en = map(int, input().split())
+        matrix[st].append(en)
+    S, G = list(map(int, input().split()))
+    stack = [S]
+    while stack:
+        check = stack.pop()
+        if not visited[check]:
+            visited[check] = 1
+            for v in matrix[check]:
+                if not visited[v]:
+                    stack.append(v)
+    print('#{} {}'.format(tc, visited[G]))

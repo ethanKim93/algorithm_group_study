79e68870131a152699c9ee21fb5c1cdcc5c0a1f5
onehousesilver <onehousesilver@naver.com>
2021-08-20 00:00:19 +0900

    조금 더 하겠습니다

--- 0819/한채은/1219_길찾기/s1.py
+++ 0819/한채은/1219_길찾기/s1.py
@@ -1,5 +1,28 @@
 import sys
 sys.stdin = open("input.txt")
 
-T, N = map(int,input().split())
-for tc in range(1, T+1):
+
+def dfs(v):
+    visited[v] = 1
+    for w in range(1, v+1):
+        if G[v][w] == 1 and not visited[w]:
+            dfs(w)
+
+
+for tc in range(1, 11):
+    v, e = map(int, input().split())
+    G = [[]*v for _ in range(v+1)]
+    visited = [0] * (v+1)
+
+    for i in range(e):
+        u, v = map(int, input().split())
+        G[u].append(v)
+
+    S, G = map(int, input().split())
+    dfs(S)
+
+    if visited(G) == 1:
+        print('#{} {}'.format(tc, 1))
+    else:
+        print('#{} {}'.format(tc, 0))
+

5390939d8a7a7ad166976f6487d3e2204d00cc11
gkuer <gkuer95@gmail.com>
2021-08-20 16:56:46 +0900

    submit_hws

--- 0820/이건희/1219_길찾기/s1.py
+++ 0820/이건희/1219_길찾기/s1.py
@@ -10,8 +10,17 @@ for _ in range(1, 11):
     ch2 = [0] * 100
 
     for i in range(n):
-        if ch1[road[2*i]] == 0:
-            ch1[[road[2*i]] = road[2*i+1]
+        if ch1[road[2 * i]] == 0:
+            ch1[road[2 * i]] = road[2 * i + 1]
         else:
-            ch2[[road[2*i]]] = road[2*i+1]
+            ch2[road[2 * i]] = road[2 * i + 1]
 
+    # 인접행렬방식
+    adj_arr = [[0]*100 for _ in range(100)]
+    for i in range(n):
+        adj_arr[road[2*i]][road[2*i+1]] = 1
+
+    # 인접리스트방식
+    adj_list = [[]*100 for _ in range(100)]
+    for i in range(n):
+        adj_list[road[2*i]].append(road[2*i+1])

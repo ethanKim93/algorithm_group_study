e0813b33c098501051daf3699a454c47ffff12d0
jyjdev <yun22287@naver.com>
2021-08-23 18:35:31 +0900

    0823_homework

--- 0823/윤지영/ex2/s1.py
+++ 0823/윤지영/ex2/s1.py
@@ -2,7 +2,7 @@ arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 N = len(arr)
 selected = [0] * N      # 사용여부 확인 배열
 
-def powerset(i):
+def powerset(i,subsum):
     if i == N :
         total = 0
         sub = []
@@ -10,16 +10,16 @@ def powerset(i):
             if sel:
                 total += arr[j]
                 sub.append(arr[j])
-        if total > 10:              # 현재까지 합이 타겟인 10보다 커지면 유망없으므로 return
+        if total > subsum:              # 현재까지 합이 타겟인 10보다 커지면 유망없으므로 return
             return
-        elif total == 10:           # 합이 10이 된다면, 해당 부분집합 출력
+        elif total == subsum:           # 합이 10이 된다면, 해당 부분집합 출력
             print(sub)
         return                  # i를 9을 호출했을 때(powerset(i+1))로 돌아감
     selected[i] = 1             # [1,0,0,0,0,0,0,0,0,0] > [1,1,0,0,0,0,0,0,0,0] > ... > [1,1,1,1,1,1,1,1,1,1] > if 분기
-    powerset(i+1)           # return 되면 돌아오는 곳
+    powerset(i+1,10)           # return 되면 돌아오는 곳
     selected[i] = 0         # 돌아오면 해당 값 0 으로 변경 [1,1,1,1,1,1,1,1,1,0]
-    powerset(i+1)           # powerset(10) -> if 분기타서 출력하고, 다시 return 되면 더 이상 호출할 게 없으니 다시 return 되서 위로 올라감 -> i 하나 줄어듦
-powerset(0)
+    powerset(i+1,10)           # powerset(10) -> if 분기타서 출력하고, 다시 return 되면 더 이상 호출할 게 없으니 다시 return 되서 위로 올라감 -> i 하나 줄어듦
+powerset(0,10)
 
 
 

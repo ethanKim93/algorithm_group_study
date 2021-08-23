9282424e474356ad0e3ca0192a101f8476f189ca
rkdms0116 <rkdms0116@gmail.com>
2021-08-23 23:39:49 +0900

    0823 HW 제출합니다/ ex2 실행 중입니다.

--- 0811/이가은/1954_달팽이_숫자/s1.py
+++ 0811/이가은/1954_달팽이_숫자/s1.py
@@ -1,2 +1,14 @@
 import sys
 sys.stdin = open('input.txt')
+
+test_cases = int(input())
+
+for test in range(test_cases):
+    n = int(input())
+
+    dx = [1, 0, -1, 0]
+    dy = [0, -1, 0, 1]
+    snail = [[0]*5]*5
+    nums = n * n
+
+    for num in range(1, nums+1):

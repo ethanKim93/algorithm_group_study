a99cc98765e4b236fbb2d3dd5b5a91d10ba52106
jyjdev <yun22287@naver.com>
2021-08-25 08:51:53 +0900

    modify 0824

--- 0824/윤지영/4881_배열_최소_합/s1.py
+++ 0824/윤지영/4881_배열_최소_합/s1.py
@@ -1,38 +1,31 @@
 import sys
 sys.stdin = open("sample_input.txt")
 
-def board_sum(s,e):
-    global N
-    result = boards[s][e]
-    answer = []
-    for _ in range(N):
-        exp_i = [s]
-        exp_j = [e]
-        for i in range(N):
-            for j in range(N):
-                if (i in exp_i) or (j in exp_j):
-                    continue
-                else:
-                    result += boards[i][j]
-                    exp_i.append(i)
-                    exp_j.append(j)
-        answer.append(result)
-    r = answer[0]
-    for k in range(len(answer)):
-        if r > answer[k]:
-            r = answer[k]
-    return r
-
+def board_sum(i):
+    global N, board_min, result
+    if board_min < result:
+        return              # 가지치기, 덧셈중인 result가 이미 최소값을 넘으면 볼 필요가 없으므로
+    if i == N:
+        if board_min > result:      # 행 i가 N이 된다는 것은, N-1번째 마지막행까지 모두 순회한것이므로 그때까지의 result값을 비교하여 최소값 갱신
+            board_min = result
+            return
+    for j in range(N):
+        if visited[j] == 0:
+            result += boards[i][j]
+            visited[j] = 1
+            board_sum(i+1)
+            result -= boards[i][j]
+            visited[j] = 0
 
 T = int(input())
+
 for tc in range(1,T+1):
     N = int(input())
     boards = [list(map(int, input().split())) for _ in range(N)]
-    ans = board_sum(0,0)
-    for i in range(N):
-        for j in range(N):
-            if ans > board_sum(i,j):
-                ans = board_sum(i,j)
-    print(ans)
+    visited = [0] * N
+    board_min = N * 9
+    result = 0
+    board_sum(0)
+    print('#{} {}'.format(tc, board_min))
 
 

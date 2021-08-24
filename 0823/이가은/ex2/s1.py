c66528b2704593aa101f9fd03cfa034d4ee53b42
rkdms0116 <rkdms0116@gmail.com>
2021-08-24 23:56:18 +0900

    0824 HW 하는중입니다..

--- 0823/이가은/ex2/s1.py
+++ 0823/이가은/ex2/s1.py
@@ -1,4 +1,36 @@
-# 부분합 10 구하기 구현해야합니다.
+
+arr = [i for i in range(1,11)]
+N = len(arr)
+sel = [0] * N
+
+def powerset(idx, K):
+    if len(sel) < N :
+        sel[idx] = 1
+        powerset(idx+1, K)
+        sel[idx] = 0
+        powerset(idx+1, K)
+    else:
+        power_sum = 0
+        for i in range(len(arr)):
+            if sel[i]:
+                power_sum += arr[i]
+        if power_sum == K:
+            for i in range(len(arr)):
+                if i == 1:
+                    print(arr[sel.index(i)], end=' ')
+                # 모르겠어요..ㅠㅠㅠㅠㅠ
+            print()
+
+powerset(0, 10)
+
+
+
+
+
+
+
+'''
+
 arr = [2,4,6]
 N = len(arr)
 sel = [0] * N
@@ -10,11 +42,21 @@ def powerset(idx):
                 print(arr[index], end=' ')
         print()
         return
+
     sel[idx] = 1
     powerset(idx+1)
 
     sel[idx] = 0
     powerset(idx+1)
 
-
 powerset(0)
+
+
+
+
+
+
+
+
+''''''
+'''

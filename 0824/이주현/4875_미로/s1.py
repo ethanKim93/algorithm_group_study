9612ec555ff79f5226fe68d107a9a3f3e6142fba
Ju Hyeon <aganga7427@gmail.com>
2021-08-24 14:19:39 +0900

    4874 4875

---
+++ 0824/이주현/4875_미로/s1.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open('sample_input.txt', 'r')
+
+def navi(row, col):
+    check.append((row, col))
+    result = 0
+    for idx in range(4):
+        if 0 <= row + delta_y[idx] < len(matrix) \
+                and 0 <= col + delta_x[idx] < len(matrix) \
+                and check[len(check) - 2] != (row + delta_y[idx], col + delta_x[idx]):
+
+            if matrix[row + delta_y[idx]][col + delta_x[idx]] == '0':
+                result += navi(row + delta_y[idx], col + delta_x[idx])
+                check.pop()
+            elif int(matrix[row + delta_y[idx]][col + delta_x[idx]]) > 1:
+                return 1
+    return result
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    matrix = [input() for _ in range(N)]
+    delta_x = [0, 0, -1, 1]
+    delta_y = [-1, 1, 0, 0]
+    check = []
+    start = 0
+    for i in range(N):
+        for j in range(N):
+            if int(matrix[i][j]) > 1:
+                start = (i, j)
+                break
+        if start:
+            break
+
+    print("#{} {}".format(tc, navi(start[0], start[1])))

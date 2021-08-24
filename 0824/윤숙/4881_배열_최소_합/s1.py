70741161ca3d0f29c74ad229af0723e329b329da
dbstnr2146 <dbstnr2146@naver.com>
2021-08-24 23:54:11 +0900

    0824 제출

--- 0824/윤숙/4881_배열_최소_합/s1.py
+++ 0824/윤숙/4881_배열_최소_합/s1.py
@@ -1,28 +1,26 @@
-# 부분집합의 합을 만드는 문제
-# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# n = 10
-# sel = [0] * n
-
-
-k=0
+#틀은 잡았는 데 구현 고민중입니다..
 def sumNumber(k,visited):
-    if k>=N:
+    if k>N:
         return
+    if k==N:
+        pass
     for i in range(N):
-        if vistied[i]==0:
+        if vistied[i]==0: #방문하지 않아서 방문할때
+            #....
             sum+=numbers[k][i]
             vistied[i]=1
+            # sumNumber(k+1,vistied)
+        else : #방문한 상태일때..
+            pass
+            #....
 
-            sumNumber(k+1,vistied)
-
-    k+=1
 import sys
-sys.stdin=open('input.txt')
-T=int(input())
+sys.stdin = open('input.txt')
+T = int(input())
 for tc in range(1,T+1):
 
     N=int(input())
     numbers=[list(map(int,input().split())) for _ in range(N)]
     vistied=[[0]*N for _ in range(N)]
-
-    sumNumber(k,vistied)
+    k = 0
+    print(sumNumber(k,vistied))

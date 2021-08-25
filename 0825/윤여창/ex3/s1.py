bfc2503cc96f3a974163bcd2c657153e19eb2220
YUNYEOCHANG <ron2003@naver.com>
2021-08-25 23:44:31 +0900

    제출

---
+++ 0825/윤여창/ex3/s1.py
@@ -0,0 +1,29 @@
+def bfs(v):
+    q = [v]
+
+    while q:
+        v = q.pop(0)
+        if not visited[v]:
+            visited[v] = 1
+            print(v, end=' ')
+            for w in range(1, V+1):
+                if adj[v][w] and not visited[w]:
+                    q.append(w)
+
+import sys
+sys.stdin = open('input.txt')
+
+V, E = map(int, input().split())
+
+edges = list(map(int, input().split()))
+
+
+adj = [[0] * (V+1) for _ in range(V+1)]
+
+for i in range(E):
+    adj[edges[i*2]][edges[i*2+1]] = 1
+    adj[edges[i*2+1]][edges[i*2]] = 1
+
+visited = [0] * (V+1)
+
+bfs(1)

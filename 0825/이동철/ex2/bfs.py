b328e012eaa7c1478c4ca01075064beaa79929cd
Felix0708 <chuls0708@gmail.com>
2021-08-25 21:41:51 +0900

    hw

--- 0825/이동철/ex2/bfs.py
+++ 0825/이동철/ex2/bfs.py
@@ -9,7 +9,7 @@ def bfs1(s, V):
     q.append(s) # 시작점 enQueue
     visited[s] = 1  # 시작점 visited 표시
     while q:    # Queue가 비어있지 않으면 (처리할 정점이 )
-        t = q.pop() # deQueue, 꺼내서 t에 저장
+        t = q.pop(0) # deQueue, 꺼내서 t에 저장
         print(t)    # t에 대한 처리
         for i in range(1, V + 1):   # t에 인접이고 미방문
             if adj[t][i] == 1 and visited[i] == 0:
@@ -23,7 +23,7 @@ def bfs2(s, V):
     q.append(s) # 시작점 enQueue
     visited[s] = 1  # 시작점 visited 표시
     while q:    # Queue가 비어있지 않으면 (처리할 정점이 )
-        t = q.pop() # deQueue, 꺼내서 t에 저장
+        t = q.pop(0) # deQueue, 꺼내서 t에 저장
         print(t)    # t에 대한 처리
         for i in adj_lst[t]:   # t에 인접이고 미방문
             if visited[i] == 0:
@@ -38,8 +38,9 @@ sys.stdin = open('input.txt')
 V, E = map(int, input().split())    # V(ertex), E(dge)
 
 edge = list(map(int, input().split()))  # 간선 정보 초기화
-# Graph 초기화
-adj = [[0] * (V + 1) for _ in range(V + 1)] # 인접행렬
+# 1. Graph 초기화 # 인접행렬 이용
+adj = [[0] * (V + 1) for _ in range(V + 1)]
+# 2. 인접리스트 이용
 adj_lst = [[] for _ in range(V + 1)]
 
 for i in range(E):
@@ -49,7 +50,9 @@ for i in range(E):
 
     adj_lst[n1].append(n2)
     adj_lst[n2].append(n1)
+
 # 방문 표시 초기화
+# visited = [0] * (V + 1)
 
 # bfs 탐색 시작
 bfs1(1, V)

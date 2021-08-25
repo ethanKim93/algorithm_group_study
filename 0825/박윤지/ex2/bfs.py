8f8f01ea568597edddac9715d91af796865f34af
underwater <shaian2@naver.com>
2021-08-25 18:01:30 +0900

    0825: 2: 제출

--- 0825/박윤지/ex2/bfs.py
+++ 0825/박윤지/ex2/bfs.py
@@ -8,11 +8,16 @@ def bfs(v):
     visited[v] = 1
     while q:
         t = q.pop(0)
-        print(t)
+        # 인접 행렬
         for i in range(1, V+1):
             if visited[i] == 0 and graph[t][i] == 1:
                 q.append(i)
                 visited[i] = visited[t] + 1
+        # # 인접 리스트
+        # for w in adjlist[t]:
+        #     if not visited[w]:
+        #         q.append(i)
+        #         visited[i] = visited[t] + 1
 
 
 import sys
@@ -25,11 +30,16 @@ V, E = map(int, input().split())
 edge = list(map(int, input().split()))
 
 # Graph 초기화
+# 인접 행렬
 graph = [[0] * (V+1) for _ in range(V+1)]
 for i in range(E):
     s, e = edge[i*2], edge[i*2 + 1]
     graph[s][e] = 1
     graph[e][s] = 1
+# # 인접 리스트
+# adjlist = [[] for _ in range(V+1)]
+# for i in range(E):
+#     adjlist[edge[i*2]].append(edge[i*2 + 1])
 
 # 방문 표시 초기화
 visited = [0]*(V+1)

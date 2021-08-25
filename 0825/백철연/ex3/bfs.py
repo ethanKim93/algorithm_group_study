08068b7b4633f5d995a6090c0306c0db3aa7c732
backcy1 <backcy855@gmail.com>
2021-08-25 17:23:31 +0900

    0825_ex3_bfs

---
+++ 0825/백철연/ex3/bfs.py
@@ -0,0 +1,54 @@
+
+"""
+bfs - 인접 행렬 or 인접 리스트 구현
+"""
+
+def bfs(v):
+
+    q = []
+    q.append(v)
+    visited[v] = 1
+    while q:
+        t = q.pop(0)
+        print(t)
+        for i in range(1, V+1):
+            if adj[t][i] == 1 and visited[i] == 0:
+                q.append(i)
+                visited[i] = visited[t] + 1
+
+#from pprint import  pprint
+import sys
+sys.stdin = open('input.txt')
+
+
+# V(ertex), E(dge)
+V, E = map(int, input().split())  # 정점, 간선
+# 간선 정보 초기화
+edge = list(map(int,input().split()))
+# Graph 초기화
+adj = [[0]*(V+1) for _ in range(V+1)]
+
+for i in range(E):
+    n1, n2 = edge[2*i], edge[2*i+1]
+    adj[n1][n2] = 1
+    adj[n2][n1] = 1
+#pprint(adj)
+
+# 방문 표시 초기화
+
+visited = [0]*(V+1)
+
+# bfs 탐색 시작
+bfs(1)
+
+# V,E = map(int, input().split())
+# edge = list(map(int,input().split()))
+# adjList = [[] for _ in range(V+1)]           # 인접 리스트
+#
+# for i in range(E):
+#     n1, n2 = edge[2*i], edge[2*i+1]
+#
+#     adjList[n1].append(n2)
+#     adjList[n2].append(n1)
+#
+# print(adjList)

10a95a5f140f75c38d31623d0642c53673b38ee9
sdh98429 <sdh98429@naver.com>
2021-08-26 14:10:50 +0900

    0826 1226_미로1 그냥 5105_미로의_거리 코드 복붙해서 풀었습니다..ㅎ

--- 0826/신동호/5099_피자굽기/s1.py
+++ 0826/신동호/5099_피자굽기/s1.py
@@ -12,7 +12,7 @@ for tc in range(1, T+1):
     while pizza: # 피자가 남아있을 때
         if not cheese[0]//2: # 돌아온 피즈치자의 양이 0일 때
             p = pizza.pop(0) # 피자를 화덕에서 꺼내고 번호 기록
-            cheese.pop(0) # 치즈를 없애준다, 더 넣어줄 피자가 없을 경우 치즈 큐의 크기는 M보다 작음
+            cheese.pop(0) # 치즈를 없애준다, 더 넣어줄 피자가 없을 경우 치즈 큐의 크기는 N보다 작음
             while C and len(pizza) < N: # 넣어줄 피자가 남아있고, 화덕이 비어있는 동안
                 pizza.append(i) # 새 피자를 넣어준다
                 cheese.append(C.pop(0)) # 새 피자 치즈의 양을 넣어준다

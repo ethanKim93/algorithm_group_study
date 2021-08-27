3925b543a5dba4f5d51b6f6c93600311c14a5d4a
sdh98429 <sdh98429@naver.com>
2021-08-27 17:30:18 +0900

    0827 러시아 국기 풀긴 풀었는데 코드가 구림

--- 0827/신동호/4613_러시아_국기_같은_깃발/s1.py
+++ 0827/신동호/4613_러시아_국기_같은_깃발/s1.py
@@ -1,9 +1,37 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
+def color(mat):
+    global result
+    for i in range(1, N-1):
+        for j in range(i+1, N):
+            subsum = 0
+            for x in range(0, i):
+                subsum += M - mat[x][0]
+            for y in range(i, j):
+                subsum += M - mat[y][1]
+            for z in range(j, N):
+                subsum += M - mat[z][2]
+            if subsum < result:
+                result = subsum
+
+
+
+
 T = int(input())
 
 for tc in range(1, T+1):
     N, M = list(map(int, input().split()))
-    matrix = [list(input()) for _ in range(N)]
-    print(matrix)
+    matrix = [[0]*3 for _ in range(N)]
+    result = N * M
+    for n in range(N):
+        inf = list(input())
+        for f in inf:
+            if f == 'W':
+                matrix[n][0] += 1
+            elif f == 'B':
+                matrix[n][1] += 1
+            elif f == 'R':
+                matrix[n][2] += 1
+    color(matrix)
+    print('#{} {}'.format(tc, result))

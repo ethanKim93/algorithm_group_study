33b6b5624828fa667e7ef4c94dfe38c1b225b8da
Leo <guswhddlf11@gmail.com>
2021-08-27 13:16:49 +0900

    0827

--- 0827/현종일/ex01/s1.py
+++ 0827/현종일/ex01/s1.py
@@ -2,19 +2,14 @@ import sys
 sys.stdin = open("input.txt")
 
 def VLR(s):
-    queue.append(s)
-    while queue:
-        t = queue.pop(0)
-        print(t, end=" ")
-        for edge in edges[t]:
-            VLR(edge)
+    print(s, end=" ")
+    for edge in edges[s]:
+        VLR(edge)
 
 v = int(input())
 e = list(map(int, input().split()))
 
 edges = [[] for _ in range(len(e))]
-
-queue = []
 
 for i in range((v-1)):
     edges[e[i*2]].append(e[i*2+1])

0e844c7ee8ed103f44f840e66adbaccf68092aec
sdh98429 <sdh98429@naver.com>
2021-08-27 16:53:43 +0900

    0827 중위 풀고 러시아 시도 중...리비아 국기면 좋았을텐데

---
+++ 0827/신동호/4613_러시아_국기_같은_깃발/s1.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = list(map(int, input().split()))
+    matrix = [list(input()) for _ in range(N)]
+    print(matrix)

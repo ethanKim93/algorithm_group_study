536df316ad7586abcb48662b5e95ba6a8d83022b
Felix0708 <chuls0708@gmail.com>
2021-09-23 20:33:05 +0900

    0923 hw 풀이 해주세요 프로님

--- 0923/이동철/5177_이진_힙/s1.py
+++ 0923/이동철/5177_이진_힙/s1.py
@@ -2,23 +2,53 @@ import sys
 sys.stdin = open('sample_input.txt')
 
 
-def min_heap(n):
-    if n > 0:
-        if tree[n//2] > tree[n]:
-            tree[n], tree[n // 2] = tree[n // 2], tree[n]
-            min_heap(n // 2)
+def change(i):  # 노드 값 비교하면서 계속 바꿔주는
+    if i != 0:
+        if nd[i//2] > nd[i]:
+            nd[i], nd[i//2] = nd[i//2], nd[i]
+            change(i//2)
 
 
-T = int(input())
+def sum_hp(N):  # 정렬된 노드 값에서 조상노드값들 더하기 함수
+    total_sum = 0
+    while N:
+        N //= 2
+        total_sum += nd[N]
+    return total_sum
+
+
+T = int(input())    # 기본 셋팅
 for tc in range(1, T + 1):
     N = int(input())
-    nodes = list(map(int, input().split()))
+    nd = [0] + list(map(int, input().split()))
+    for i in range(1, len(nd)):
+        change(i)
 
-    tree = [0] * (N+1)
-    for i in range(0, N):
-        tree[i+1] = nodes[i]
-    for num in range(1, len(tree)):
-        min_heap(num)
+    print('#{} {}'.format(tc, sum_hp(N)))
 
-    total = 0
-    
+
+
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N = int(input())
+#     NList = [0] + list(map(int, input().split()))
+#     # 이진최소힙 만들기
+#     tree = [0] * (N+1)
+#     tree[1] = NList[1]
+#     for i in range(2, N+1):
+#         tree[i] = NList[i]
+#         while i != 1:
+#             if tree[i] < tree[i//2]:
+#                 temp = tree[i]
+#                 tree[i] = tree[i//2]
+#                 tree[i//2] = temp
+#                 i //= 2
+#             else:
+#                 break
+#     # 조상 노드의 합 구하기
+#     total = 0
+#     while N != 0:
+#         N //= 2
+#         total += tree[N]
+#     print('#{} {}'.format(tc, total))

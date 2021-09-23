003938ad8afdc72fc23008a91b408a1e910417cc
Felix0708 <chuls0708@gmail.com>
2021-09-23 21:05:50 +0900

    0923 hw 풀이 부탁드립니다...ㅠ

--- 0923/이동철/1232_사칙연산/s1.py
+++ 0923/이동철/1232_사칙연산/s1.py
@@ -2,41 +2,40 @@ import sys
 sys.stdin = open('input.txt')
 
 
-def postorder(v):  # 후위 순회
-    if v >= N:
-        return
-    if tree[v][2] > 0:
-        postorder(tree[v][2])  # 왼쪽 자식
-    if tree[v][3] > 0:
-        postorder(tree[v][3])  # 오른쪽 자식
-
-    # 연산자에 계산 결과 업데이트해야 다음 연산자 노드에서 계산 가능
-    if tree[v][1] == '+':
-        # 왼쪽 노드와 오른쪽 노드 더하기
-        tree[v][1] = tree[tree[v][2]][1] + tree[tree[v][3]][1]
-    elif tree[v][1] == '-':
-        # 왼쪽 노드와 오른쪽 노드 뺄셈
-        tree[v][1] = tree[tree[v][2]][1] - tree[tree[v][3]][1]
-    elif tree[v][1] == '*':
-        # 왼쪽 노드와 오른쪽 노드 곱셈
-        tree[v][1] = tree[tree[v][2]][1] * tree[tree[v][3]][1]
-    elif tree[v][1] == '/':
-        # 왼쪽 노드와 오른쪽 노드 뺄셈
-        tree[v][1] = tree[tree[v][2]][1] / tree[tree[v][3]][1]
-
-
-for tc in range(1, 11):  # 10개의 테스트 케이스
-    N = int(input())  # 정점의 총 수
-    tree = [[0] * 4 for _ in range(N + 1)]  # 1 인덱스부터 시작해서 N+1
-    # 트리 입력
-    for n in range(N):
-        info = list(input().split())
-        if info[1].isdigit():  # 숫자면
-            tree[int(info[0])][1] = int(info[1])
+def post_order(n):
+    global stack
+    if tree[n]:
+        post_order(left[n])
+        post_order(right[n])
+        if type(tree[n]) == int:
+            stack += [tree[n]]
         else:
-            tree[int(info[0])][1] = info[1]  # 연산자
-            tree[int(info[0])][2] = int(info[2])  # 왼쪽 자식
-            tree[int(info[0])][3] = int(info[3])  # 오른쪽 자식
+            a = stack.pop()
+            b = stack.pop()
+            if tree[n] == '+':
+                stack += [b + a]
+            elif tree[n] == '-':
+                stack += [b - a]
+            elif tree[n] == '*':
+                stack += [b * a]
+            elif tree[n] == '/':
+                stack += [b / a]
 
-    postorder(1)
-    print("#{} {}".format(tc, int(tree[1][1])))
+
+for tc in range(1, 11):
+    N = int(input())
+    tree = [0] * (N + 1)
+    left = [0] * (N + 1)
+    right = [0] * (N + 1)
+    for _ in range(N):
+        temp = input().split()
+        if len(temp) == 4:
+            tree[int(temp[0])] = temp[1]
+            left[int(temp[0])] = int(temp[2])
+            right[int(temp[0])] = int(temp[3])
+        else:
+            tree[int(temp[0])] = int(temp[1])
+
+    stack = []
+    post_order(1)
+    print('#{} {}'.format(tc, int(stack[0])))

879daa5a7bca524ac241d3fae609fb5b1cecf8d8
Ju Hyeon <aganga7427@gmail.com>
2021-09-23 16:55:58 +0900

    5178

--- 0923/이주현/5178_노드의합/s1.py
+++ 0923/이주현/5178_노드의합/s1.py
@@ -3,6 +3,13 @@ sys.stdin = open('sample_input.txt')
 
 T = int(input())
 
+def post(idx, N):
+    result = 0
+    if idx <= N:
+        result = tree[idx] + post(2*idx, N) + post(2*idx + 1, N)
+        tree[idx] = result
+    return result
+
 for tc in range(1, T + 1):
     N, M, L = map(int, input().split())
     tree = [0] * (N + 1)
@@ -10,5 +17,6 @@ for tc in range(1, T + 1):
     for i in range(M):
         node, value = map(int, input().split())
         tree[node] = value
+    post(1, N)
 
-    print(tree)
+    print("#{} {}".format(tc, tree[L]))

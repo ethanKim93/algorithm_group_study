e574dce8adbc32335af99179c4144e78fb9ca81f
82surf <82surf@gmail.com>
2021-09-23 19:51:12 +0900

    add 1232

--- 0923/유현수/1232_사칙연산/s1.py
+++ 0923/유현수/1232_사칙연산/s1.py
@@ -1,3 +1,43 @@
+# 직접 못 풀어서 동호님 코드 보고 이해했습니다!
+
 import sys
 sys.stdin = open('input.txt')
 
+
+def post_order(n):
+    global stack
+    if tree[n]:
+        post_order(left[n])
+        post_order(right[n])
+        if type(tree[n]) == int:
+            stack += [tree[n]]
+        else:
+            a = stack.pop()
+            b = stack.pop()
+            if tree[n] == '+':
+                stack += [b + a]
+            elif tree[n] == '-':
+                stack += [b - a]
+            elif tree[n] == '*':
+                stack += [b * a]
+            elif tree[n] == '/':
+                stack += [b / a]
+
+
+for tc in range(1, 11):
+    N = int(input())
+    tree = [0] * (N + 1)
+    left = [0] * (N + 1)
+    right = [0] * (N + 1)
+    for _ in range(N):
+        temp = input().split()
+        if len(temp) == 4:
+            tree[int(temp[0])] = temp[1]
+            left[int(temp[0])] = int(temp[2])
+            right[int(temp[0])] = int(temp[3])
+        else:
+            tree[int(temp[0])] = int(temp[1])
+
+    stack = []
+    post_order(1)
+    print('#{} {}'.format(tc, int(stack[0])))
---
+++ 0923/유현수/5178_노드의_합/s2.py
@@ -0,0 +1,18 @@
+# 최종코드(이유현)
+
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M, L = map(int, input().split())
+    tree = [0] * (N+1)
+
+    for _ in range(M):
+        temp = list(map(int, input().split()))
+        tree[temp[0]] = temp[1]
+
+    for idx in range(N, 1, -1):
+        tree[idx//2] += tree[idx]
+
+    print('#{} {}'.format(tc, tree[L]))

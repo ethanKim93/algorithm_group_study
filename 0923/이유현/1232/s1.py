55fd794f2273a2db965bc5942a1f2d14bba82410
julie0919 <julie.lee0919@gmail.com>
2021-09-23 18:10:56 +0900

    homework

--- 0923/이유현/1232/s1.py
+++ 0923/이유현/1232/s1.py
@@ -3,13 +3,30 @@ sys.stdin = open('input.txt')
 
 for tc in range(1, 11):
     N = int(input())
-    tree = [0] * (N+1)
+    tree = [[] for _ in range(N+1)]
+    num_tree = [0] * (N+1)
 
     for _ in range(N):
         temp = list(input().split())
         if len(temp) > 2:
-            tree[int(temp[0])] = temp[1]
-        else:
-            tree[int(temp[0])] = int(temp[1])
+            tree[int(temp[0])].append(temp[1])
+            tree[int(temp[0])] += list(map(int, temp[2:]))
 
-    
+        else:
+            num_tree[int(temp[0])] = int(temp[1])
+
+    for t in range(N, 0, -1):
+        if tree[t]:
+            opt = tree[t].pop(0)
+            idx1 = tree[t].pop(0)
+            idx2 = tree[t].pop(0)
+            if opt == '+':
+                num_tree[t] = num_tree[idx1] + num_tree[idx2]
+            elif opt == '-':
+                num_tree[t] = num_tree[idx1] - num_tree[idx2]
+            elif opt == '*':
+                num_tree[t] = num_tree[idx1] * num_tree[idx2]
+            elif opt == '/':
+                num_tree[t] = num_tree[idx1] // num_tree[idx2]
+
+    print('#{} {}'.format(tc, num_tree[1]))

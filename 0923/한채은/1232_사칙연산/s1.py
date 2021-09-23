6d538b955fcfc9f74e99aad9511b8eae22f764f0
onehousesilver <onehousesilver@naver.com>
2021-09-24 01:20:22 +0900

    hws

--- 0923/한채은/1232_사칙연산/s1.py
+++ 0923/한채은/1232_사칙연산/s1.py
@@ -2,32 +2,31 @@ import sys
 sys.stdin = open('input.txt')
 
 
-def result(operator):
-    if operator == '+':
-        ans = tree[int(inform[i][2])] + tree[int(inform[i][3])]
-    elif operator == '-':
-        ans = tree[int(inform[i][2])] - tree[int(inform[i][3])]
-    elif operator == '*':
-        ans = tree[int(inform[i][2])] * tree[int(inform[i][3])]
-    else:
-        ans = tree[int(inform[i][2])] // tree[int(inform[i][3])]
+def change(op):
+    if op == '+':
+        ans = tree[int(info[i][2])] + tree[int(info[i][3])]
+    if op == '-':
+        ans = tree[int(info[i][2])] - tree[int(info[i][3])]
+    if op == '*':
+        ans = tree[int(info[i][2])] * tree[int(info[i][3])]
+    if op == '/':
+        ans = tree[int(info[i][2])] // tree[int(info[i][3])]
     return ans
 
 
-# 연산자를 판별할 리스트
 cal = ['+', '-', '*', '/']
 
 for tc in range(1, 11):
     N = int(input())
-    tree = [0] * (N + 1)
-    inform = [0] + [list(input().split()) for _ in range(N)]
+    info = [0] + [list(input().split()) for _ in range(N)]
+    tree = [0] * (N+1)
 
-    # 거꾸로 돌면서 연산자가 나오면 함수로 계산
+
+    # 1까지만 필요하니까 범위는 0까지
     for i in range(N, 0, -1):
-        if inform[i][1] in cal:
-            tree[i] = result(inform[i][1])
-        # 연산자가 아니면 트리에 값 집어 넣기
+        if info[i][1] in cal:
+            tree[i] = change(info[i][1])
         else:
-            tree[i] = int(inform[i][1])
+            tree[i] = int(info[i][1])
 
     print('#{} {}'.format(tc, tree[1]))

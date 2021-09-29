dd2b965aa6de00abdc57d53c0ead910bdc7c4d33
jyjdev <yun22287@naver.com>
2021-09-29 13:24:33 +0900

    ex1

---
+++ 0929/윤지영/ex1/s1.py
@@ -0,0 +1,15 @@
+li = input()
+num = [[] for _ in range(len(li)//7)]
+j = 0
+for i in range(0,len(li)-6,7):
+    num[j]=li[i:i+7]
+    j+=1
+
+for k in range(len(num)):
+    l = len(num[k])
+    result = s = 0
+    for m in range(l-1,-1,-1):
+        result += 2**s * int(num[k][m])
+        s += 1
+    print(result, end=' ')
+

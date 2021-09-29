7f645ae9dd0e89eab9e822c1c8280d28d8cc20f1
dbstnr2146 <dbstnr2146@naver.com>
2021-09-29 14:52:39 +0900

    0929_수업]

---
+++ 0929/윤숙/ex1/s1.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.stdin = open('input.txt')
+binaryarr = input()
+k = 7
+for i in range(0, len(binaryarr), k):
+    result = int(binaryarr[i:k + i], 2)
+    print(result, end=' ')
+
+print()
+for i in range(0, len(binaryarr), k):
+    num = binaryarr[i:k + i]
+    cac = 0
+    for j in range(k):
+        if num[j] == "1":
+            cac += 2 ** ((k-1)-j)
+    print(cac, end=' ')

b6b928c4e90c63e52e91feb6830f804ebb6d5a34
julie0919 <julie.lee0919@gmail.com>
2021-09-29 18:04:15 +0900

    0929_ex3

--- 0929/이유현/ex2/s1.py
+++ 0929/이유현/ex2/s1.py
@@ -1,12 +1,12 @@
 # hex_str = '0F97A3'
 hex_str = '01D06079861D79F99F'
 
-hex_bin = []
+hex_dec = []
 for hex in hex_str:
-    hex_bin.append(int(hex, 16))
+    hex_dec.append(int(hex, 16))
 
 bin_str = ''
-for bin in hex_bin:
+for bin in hex_dec:
     bin_str += '{0:b}'.format(bin).zfill(4)
 
 for i in range(0, len(bin_str), 7):

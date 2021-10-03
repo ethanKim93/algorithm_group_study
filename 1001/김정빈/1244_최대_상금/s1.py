9cf57cd15b6dc11712e08e6eae70eba46568af93
jpNew-man <jaengpin@gmail.com>
2021-10-03 22:21:06 +0900

    hw제출

--- 1001/김정빈/1244_최대_상금/s1.py
+++ 1001/김정빈/1244_최대_상금/s1.py
@@ -1,40 +1,46 @@
-import sys
-sys.stdin = open("input.txt")
-#1244. [S/W 문제해결 응용] 2일차 - 최대 상금
+# #1244. [S/W 문제해결 응용] 2일차 - 최대 상금
 T = int(input())
 for tc in range(1, T+1):
-    arr, C = input().split()
-    arr = list(arr)
-    n = len(arr)
-    print(arr)
-    for i in range(n):
-        mx = i
-        for j in range(i, n):
-            if
+    n, c = input().split()
+    n = list(n)
+    stn = sorted(n, reverse=True)
+    res = ''
 
+    cnt = int(c)
+    idx = 0
+    while cnt > 0:
+        maxI = idx
+        lct = 0
+        if n == stn:
+            break
+        for j in range(idx+1, len(n)):
+            if n[j:len(n)].count(max(n[j:len(n)])) > 1: # 바꿀 범위 내에 바꿀 가장 큰 수가 중복일 때
+                for k in n[j:j+cnt]: # 앞으로 바꿀 범위 내에 바꿀 값보다 작은 값이 있으면
+                    if k < n[idx]:
+                        lct += 1 # 작은것들의 개수를 셈
+            else:
+                if n[maxI] <= n[j]:
+                    maxI = j-lct
+        if maxI != idx:
+            n[maxI], n[idx] = n[idx], n[maxI]
+            cnt -= 1
+        idx += 1
 
-
-"""
-123 1
-2737 1
-757148 1
-78466 2
-32888 2
-777770 5
-436659 2
-431159 7
-112233 3
-456789 10
-"""
-"""
-#1 321
-#2 7732
-#3 857147
-#4 87664
-#5 88832
-#6 777770
-#7 966354
-#8 954311
-#9 332211
-#10 987645
-"""
+    def check(n):
+        global cnt
+        if cnt:
+            for i in n:
+                if n.count(i) > 1:
+                    cnt = 0
+                    return
+    check(n)
+    def last_check(n):
+        global cnt
+        if cnt:
+            if cnt%2:
+                n[-1], n[-2] = n[-2], n[-1]
+                cnt = 0
+            else:
+                cnt = 0
+    last_check(n)
+    print('#{} {}'.format(tc, res.join(n)))

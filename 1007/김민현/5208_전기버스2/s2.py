60e4c2ba5feabaf3e14b55875c89da5f0dbe2591
ethanKim93 <wsmh.kim12@gmail.com>
2021-10-07 22:22:56 +0900

    동철님 문제 풀지 못하고 인터넷 참고했습니다 ㅠ

---
+++ 1007/김민현/5208_전기버스2/s2.py
@@ -0,0 +1,33 @@
+# 이주현
+T = int(input())
+
+
+def BT(now, count):
+    global result
+    count += 1
+    # 횟수가 최소값을 넘어가면 중지
+    if result < count:
+        return
+
+    # 끝에 도달할 경우
+    if now + battery[now] + 1 >= len(battery):
+        # 최솟값 갱신
+        if result > count:
+            result = count
+            return
+
+    # 현 위치에서 갈 수 있는 인덱스 범위 지정
+    can = range(now + 1, now + battery[now] + 1)
+    for c in can:
+        BT(c, count)
+
+
+for tc in range(1, 1 + T):
+    tmp = list(map(int, input().split()))
+    N = tmp[0]  # 정류장 개수
+    battery = tmp[1:] + [0]  # 정류장별 배터리 용량
+    result = N
+
+    for n in range(1, len(battery[:battery[0]]) + 1):
+        BT(0 + n, 0)
+    print("#{} {}".format(tc, result))
---
+++ 1007/김민현/5209_최소 생산 비용/s1.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+
+
+def price(fact):
+    for pro in range(N):
+        if possible(fact,pro):
+            vi.append(fact,pro)
+    else:
+
+def possible()
+
+for tc in range(1,T+1):
+    N = int(input())
+    factories = [list(map(int,input().split())) for _  in range(N)]
+
+    factory = []# 방문한 공장
+    vi = [] #idx 공장의 생산 가격
+
+    print('#{} {}'.format(tc,factory))

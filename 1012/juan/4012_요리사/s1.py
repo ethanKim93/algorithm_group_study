c553514b119fc37a7a9d31d8a2f4a407515c9f00
juan <hphk.juan@gmail.com>
2021-10-12 15:40:27 +0900

    Problem_Solving

--- 1012/juan/4012_요리사/s1.py
+++ 1012/juan/4012_요리사/s1.py
@@ -1,6 +1,6 @@
 def cook(forA, forB, half):
     A = B = 0
-    for i in range(half-1):
+    for i in range(half-1):                     # forA, forB 배열의 모든 조합
         for j in range(i, half):
             A += ingredients[forA[i]][forA[j]] + ingredients[forA[j]][forA[i]]
             B += ingredients[forB[i]][forB[j]] + ingredients[forB[j]][forB[i]]
@@ -12,14 +12,14 @@ def combination(half):
     for i in range(1, 2**N):
         cnt, used, unused = 0, [], []
         for j in range(N):
-            if i & (1 << j):
+            if i & (1 << j):                    # A요리를 위해 사용한 식재료
                 cnt += 1
                 used.append(j)
-            else:
+            else:                               # A요리에 사용되지 않은 식재료 (= B요리에 사용한 식재료)
                 unused.append(j)
-        if cnt == half:
-            gap = cook(used, unused, cnt)
-            if gap < similar:
+        if cnt == half:                         # A요리에 사용한 식재료가 전체 식재료의 절반인 경우만
+            gap = cook(used, unused, cnt)       # A요리와 B요리의 맛의 차이를 gap에 저장
+            if gap < similar:                   # 전역 similar변수의 값보다 작으면 갱신
                 similar = gap
 
 

c936472d491af2969a0eb8c97ab4dec56e66a930
82surf <82surf@gmail.com>
2021-10-13 23:59:45 +0900

    ex4 구현 완료

--- 1013/유현수/ex4/s1.py
+++ 1013/유현수/ex4/s1.py
@@ -1,13 +1,11 @@
 import sys
-sys.stdin = open('sample_input.txt')
+sys.stdin = open('input.txt')
 
 
 def dijkstra(s):
     route = [0] * (V+1)
     route[s] = 1
-    print(s, end=' ')
     cost = adj[s][:]                    # 시작점에서 해당 인덱스의 정점까지의 비용을 기록하는 배열
-    # print(cost)
 
     for _ in range(V):
         w = 0
@@ -18,7 +16,6 @@ def dijkstra(s):
                     w = i
                     minV = cost[w]
         route[w] = 1                    # 정점 w 선택
-        print(w, end=' ')
         for v in range(V+1):
             if adj[w][v]:               # w에 인접한 정점 v에 대해
                 cost[v] = min(cost[v], cost[w] + adj[w][v])
@@ -26,7 +23,7 @@ def dijkstra(s):
                 # D[w] == 시작점에서 정점 w까지 필요한 비용
                 # adj[w][v] = 정점 w에서 정점 v까지 필요한 비용
                 # 즉, 현재 시작점에서 v까지 필요한 비용 vs 현재 시작점 - w - v까지의 비용 중 적은 것을 D에 기록한다는 뜻
-                # print(cost)
+    return cost
 
 
 T = int(input())
@@ -41,5 +38,6 @@ for tc in range(1, T+1):
         adj[j][j] = 0
 
     print('#{}'.format(tc), end=' ')
-    dijkstra(0)
+    for i in range(V+1):
+        print(dijkstra(0)[i], end=' ')
     print()

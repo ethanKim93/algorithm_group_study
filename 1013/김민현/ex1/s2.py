471e6333052b77462ca2ce65f0079a3bae82f3d1
ethanKim93 <wsmh.kim12@gmail.com>
2021-10-13 19:19:52 +0900

    1013 hws

---
+++ 1013/김민현/ex1/s2.py
@@ -0,0 +1,17 @@
+#반복
+
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    data = list(map(int, input().split()))
+    nodes = [list() for _ in range(len(data) // 2)]
+    for i in range(0, len(data), 2):
+        nodes[data[i]].append(data[i + 1])
+    visited = [False] * 9
+    ans_list = []
+
+    dfs(nodes, 1, visited)
+    print(*ans_list)

551f752f8d3ecc32ee323a32dacbb574e78311a9
Leo <guswhddlf11@gmail.com>
2021-10-14 22:19:25 +0900

    1014

--- 1014/현종일/5251_최소_이동_거리/s1.py
+++ 1014/현종일/5251_최소_이동_거리/s1.py
@@ -1,33 +1,26 @@
 import sys
 sys.stdin = open("sample_input.txt")
 
-
 for tc in range(1, int(input())+1):
     N, E = map(int, input().split())
     distance = [0] + [987654321 for _ in range(N)]
-    node = [[] for _ in range(N + 1)]
-    visited = [0] * (N+1)
+    nodes = [[] for _ in range(N+1)]
 
     for _ in range(E):
-        tmp1, tmp2, w = map(int, input().split())
-        node[tmp1].append((tmp2, w))
+        n1, n2, w = map(int, input().split())
+        nodes[n1].append((n2, w))
 
     queue = [(0, 0)]
 
     while queue:
         idx, d = queue.pop(0)
 
-        if visited[idx]:
-            continue
-
-        visited[idx] = 1
-        for n_idx, nd in node[idx]:
+        for node in nodes[idx]:
+            n_idx, nd = node
             if distance[n_idx] > d + nd:
                 distance[n_idx] = d + nd
                 queue.append((n_idx, distance[n_idx]))
 
-    print(distance[N])
-
-
+    print("#{} {}".format(tc, distance[N]))
 
 

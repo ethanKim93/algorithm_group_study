f8110396bfd1b9c36c641b6a607895dd6c957a95
ethanKim93 <wsmh.kim12@gmail.com>
2021-10-15 00:29:15 +0900

    1014hws

---
+++ 1014/김민현/5248_그룹나누기/s1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+
+for tc in range(1,T+1):
+    N,M = map(int,input().split())
+    nums = [i for _ in range(N+1)]
+    paper = list(map(int,input().split()))
+    ans =
+    for idx,i in enumerate(nums):
+        try:
+            nums.remove(i)
+        except:
+            pass
+
+        if idx%2:
+
+        else:

eef67b1f2fab5741bd53a0835ad95a32be5c4464
Sagytic <jb9992@daum.net>
2021-10-17 17:47:33 +0900

    1015ing

--- 1015/장지빈/1795_인수의_생일_파티/s1.py
+++ 1015/장지빈/1795_인수의_생일_파티/s1.py
@@ -1,8 +1,23 @@
 import sys
 sys.stdin = open('input.txt')
 
-def dijkstra():
-    pass
+def dijkstra(start, adj):
+    visited = [0]*(N+1)
+    dist = [INF]*(N+1)
+    dist[start] = 0
+    visited[start] = 1
+    val = INF
+    idx = 0
+    for i in range(1, N+1):
+        if dist[i] < val and not visited[i]:
+            val = dist[i]
+            idx = i
+    for j in range(N+1):
+        if adj[idx][j] < INF:
+            if dist[j] > dist[idx] + dist[idx][j]:
+                dist[j] = dist[idx] + dist[idx][j]
+    return dist
+
 
 for tc in range(1, int(input())+1):
     ans = 0
@@ -18,10 +33,15 @@ for tc in range(1, int(input())+1):
 
     dist1 = [INF]*(N+1)
     dist2 = [INF]*(N+1)
+    a = dijkstra(X, adj1)
+    b = dijkstra(X, adj2)
+    print(a, b)
+
 
     max_value = 0
     for i in range(1, N+1):
         if dist1[i] + dist2[i] > max_value:
             max_value = dist1[i] + dist2[i]
+
 
     print('#{} {}'.format(tc, max_value))
